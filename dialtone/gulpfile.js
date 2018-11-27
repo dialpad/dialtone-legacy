7107b08152234eb96fbde6ea6931ce755175f37d
Joshua Hynes <hello@joshuahynes.com>
2018-11-27 14:09:30 -0500

    Add in Jekyll, update Gulp, and start building out pages.

--- dialtone/gulpfile.js
+++ dialtone/gulpfile.js
@@ -1,21 +1,26 @@
+const child = require('child_process');
+const browserSync = require('browser-sync').create();
+
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const cssorder = require('postcss-ordered-values');
 const utility = require('postcss-utilities');
-const browserSync = require('browser-sync').create();
 const postcssPresentEnv = require('postcss-preset-env');
 const postcssNormalize = require('postcss-normalize');
 const cssnano = require('cssnano');
+const gutil = require('gulp-util');
 
-gulp.task('browserSync', function() {
-    browserSync.init({
-        watch: true,
-        port: 8080,
-        server: "./"
-    });
-});
-gulp.task('sass', function() {
+const siteRoot = "_site";
+const cssSource = "**/*.scss";
+const cssAppFile = "assets/scss/dialtone.scss"
+const htmlSource = "*.html";
+const dataSource = "*.yml";
+const jsSource = "**/*.js"
+const cssDest = "assets/css/";
+
+
+gulp.task('css', function() {
     var plugins = [
         postcssNormalize(),
         postcssPresentEnv(),
@@ -23,20 +28,57 @@ gulp.task('sass', function() {
         utility,
         cssnano
     ]
-    return gulp.src('assets/scss/app.scss')
+    return gulp.src(cssAppFile)
         .pipe(sass().on('error', sass.logError))
         .pipe(postcss(plugins))
-        .pipe(gulp.dest('assets/css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }));
+        .pipe(gulp.dest(cssDest))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function() {
-    gulp.watch('assets/scss/**/*.scss', ['sass']).on('change', function() {
-        browserSync.reload();
+gulp.task('js', function() {
+    return gulp.src(jsSource)
+        .pipe(browserify())
+        .pipe(gulp.dest("assets/dist/js/"));
+})
+
+gulp.task('css-minify', function() {
+
+})
+
+gulp.task('jekyll', () => {
+    const jekyll = child.spawn('jekyll', ['build',
+        '--watch',
+        '--incremental',
+        '--drafts'
+    ]);
+    const jekyllLogger = (buffer) => {
+        buffer.toString()
+            .split(/\n/)
+            .forEach((message) => gutil.log('Jekyll: ' + message));
+    };
+
+    jekyll.stdout.on('data', jekyllLogger);
+    jekyll.stderr.on('data', jekyllLogger);
+});
+
+gulp.task('serve', function() {
+    browserSync.init({
+        files: [siteRoot + '/**'],
+        watch: true,
+        port: 4000,
+        open: false,
+        server: {
+            baseDir: siteRoot
+        }
     });
-    gulp.watch('*.html', browserSync.reload);
+
+    gulp.watch(cssSource, ['css']);
+    gulp.watch(htmlSource).on('change', function() {
+        gulp.task('jekyll');
+    });
+    gulp.watch(dataSource).on('change', function() {
+        gulp.task('jekyll');
+    });
 });
 
-gulp.task( 'default', ['watch'] );
+gulp.task('default', ['css', 'jekyll', 'serve']);
--- dialtone/index.js
+++ dialtone/index.js
@@ -1,1 +1,9 @@
-console.log('testing');
+$(document).ready(function() {
+    var logo = $(".js-dds-logo");
+    // var header = $(".js-dds-site-header");
+    var nav = $(".js-dds-site-nav");
+    // var footer = $(".js-dds-site-footer");
+
+    logo.load("/includes/logo.html");
+    nav.load("/includes/site-nav.html");
+});
