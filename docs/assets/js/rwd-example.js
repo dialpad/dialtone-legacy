89c4d43be9faa18628828f18395cb41f2dbbaa94
Joshua Hynes <hello@joshuahynes.com>
2018-12-14 14:44:54 -0500

    Update box shadows with classes and responsive class example.

--- docs/assets/js/rwd-example.js
+++ docs/assets/js/rwd-example.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
     var btn = $(".js-example-rwd-btn");
     var example = $(".js-example-rwd-object");
-    var oldClasses = ("br-xl btr-xl bbr-xl brr-xl blr-xl ba bt bb bl br");
+    var brClasses = ("br-xl btr-xl bbr-xl brr-xl blr-xl");
+    var baClasses = ("ba bt bb bl br");
+    var bsClasses = ("bs-sm bs-md bs-lg bs-xl bs-none");
     var bgcClasses = ("bgc-blue bgc-blue-dark bgc-navy bgc-navy-dark bgc-teal");
 
     btn.on("click", function(e) {
         var newClass = $(this).data("class");
         var bgc = $(this).data("background");
+        var type = example.data("example");
 
         e.preventDefault();
         e.stopPropagation();
 
         btn.removeClass("dds-docs--example-btn__selected");
         $(this).addClass("dds-docs--example-btn__selected");
-        
-        example
-            .removeClass(oldClasses)
-            .removeClass(bgcClasses)
-            .addClass(newClass)
-            .addClass(bgc);
+
+        if (type == "br") {
+            example
+                .removeClass(brClasses)
+                .removeClass(bgcClasses)
+                .addClass(newClass)
+                .addClass(bgc);
+        }
+        else if (type == "ba") {
+            example
+                .removeClass(baClasses)
+                .addClass(newClass);
+        }
+        else if (type == "bs") {
+            example
+                .removeClass(bsClasses)
+                .addClass(newClass)
+        }
     });
 });
