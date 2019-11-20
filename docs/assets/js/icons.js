f55c352adffb7c30c82fa9db4497eb687ee8fda7
Joshua Hynes <hello@joshuahynes.com>
2019-11-20 14:41:19 -0500

    Add in descriptions for icons

--- docs/assets/js/icons.js
+++ docs/assets/js/icons.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
-    var card = $('.js-dialtone-icon-card');
-    var footer = $('.js-dialtone-icon-card-footer');
     var body = $('body');
+    var card = $('.js-dialtone-icon-card');
+    var footer = card.closest('.js-dialtone-icon-card-footer');
+    var vue = $('.js-vue-file');
+
+    $(vue).text(function() {
+        return "<" + $(this).text() + " /\>";
+    })
+
 
     $(card).on('click', function(e) {
         var allCards = card.not(this);
@@ -10,10 +16,17 @@ $(document).ready(function() {
         allCards.attr('data-selected', 'no');
 
         if (attr === "no") {
-            $(this).attr('data-selected', 'yes');
+            $(this)
+                .attr('data-selected', 'yes')
+                .css({
+                    'position': 'absolute',
+                    'top': '0',
+                    'left': '0',
+                    'bottom': '0'
+                });
         }
         else if (attr === "yes")  {
-            $(this).attr('data-selected', 'no');
+            $(this).attr('data-selected', 'no').attr('style','');
         }
     });
 
@@ -21,7 +34,7 @@ $(document).ready(function() {
         var target = $(e.target);
 
         if (!target.closest(card).length && (card).is(':visible')) {
-            $(card).attr('data-selected', 'no');
+            $(card).attr('data-selected', 'no').attr('style','');
         }
     });
 });
