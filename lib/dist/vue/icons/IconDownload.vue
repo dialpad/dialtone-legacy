51571be0cb2bbef10c1ceba9215445c3d2e46d48
Joshua Hynes <hello@joshuahynes.com>
2020-02-20 21:26:50 -0500

    Re-add missing dist files

---
+++ lib/dist/vue/icons/IconDownload.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg aria-hidden="true" aria-label="Download" class="d-svg d-svg--system d-svg__download" viewBox="0 0 24 24"><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/></svg>
+</template>
