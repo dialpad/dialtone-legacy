63876e7b1fca2649a32e8244f227694b9ee85efd
“vcheng-dialpad <vicky.cheng@dialpad.com>
2020-06-23 13:58:20 -0700

    Add Vicky Cheng as a Contributor

--- lib/dist/vue/icons/IconAnalytics.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <svg aria-hidden="true" aria-label="Analytics" class="d-svg d-svg--system d-svg__analytics" viewBox="0 0 24 24"><path d="M21 13V7h-6l2.307 2.307L13.5 13.48l-4-4L2 16.99l1.5 1.5 6-6.01 4 4 5.287-5.693L21 13z"/></svg>
-</template>
