1e1d3ba276b4078e38dbe8233b6404ea4d9383c9
Joshua Hynes <hello@joshuahynes.com>
2020-06-25 09:54:08 -0400

    Re-add icons

---
+++ lib/dist/vue/icons/IconAddCircleOutline.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg aria-hidden="true" aria-label="Add Circle Outline" class="d-svg d-svg--system d-svg__addCircleOutline" viewBox="0 0 24 24"><path d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
+</template>
---
+++ lib/dist/vue/icons/IconArrowUpward.vue
@@ -0,0 +1,3 @@
+<template>
+  <svg aria-hidden="true" aria-label="Arrow Upward" class="d-svg d-svg--system d-svg__arrowUpward" viewBox="0 0 24 24"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>
+</template>
