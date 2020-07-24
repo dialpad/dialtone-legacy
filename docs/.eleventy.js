39b250ece2a355fbee0969c31556a9eafa3a67e3
Joshua Hynes <hello@joshuahynes.com>
2020-07-24 17:13:04 -0400

    Rendering icons correctly

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -12,9 +12,31 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('assets/images');
     eleventyConfig.addPassthroughCopy('assets/js');
 
+    //  Icon shortcode
+    //  Re-used (with love) from Stack Overflow's Stacks. Extended for our purposes
+    eleventyConfig.addLiquidShortcode("icon", function(name, folder, classes, dimension) {
+        var fs = require("fs");
+        var path = "_includes/icons/" + folder + "/" + name + ".svg";
+        var svg = fs.readFileSync(path).toString("utf-8");
+        var defaultClasses = "d-svg" + name;
+
+        // If we have classes, add them
+        if (classes != null) {
+            svg = svg.replace(defaultClasses, defaultClasses + " " + classes);
+        }
+
+        // If we need to change the size, do that too
+        if (dimension != null) {
+            svg = svg.replace('width="18" height="18"', 'width="' + dimension + '" height="' + dimension + '"');
+        }
+
+        return svg;
+    });
+
+
     return {
         dir: {
             css: "assets/css"
         }
     }
-};
+}
