586b46f260c9a685bbc8f113d0fcb2dc24f20bdd
Joshua Hynes <hello@joshuahynes.com>
2020-07-27 17:05:14 -0400

    Finish icon section

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -18,7 +18,7 @@ module.exports = function(eleventyConfig) {
         var fs = require("fs");
         var path = "_includes/icons/" + folder + "/" + name + ".svg";
         var svg = fs.readFileSync(path).toString("utf-8");
-        var defaultClasses = "d-svg" + name;
+        var defaultClasses = "d-svg__" + name;
 
         // If we have classes, add them
         if (classes != null) {
@@ -27,7 +27,7 @@ module.exports = function(eleventyConfig) {
 
         // If we need to change the size, do that too
         if (dimension != null) {
-            svg = svg.replace('width="18" height="18"', 'width="' + dimension + '" height="' + dimension + '"');
+            svg = svg.replace('viewBox="0 0 24 24"', 'viewBox="0 0' + dimension + dimension + '"');
         }
 
         return svg;
