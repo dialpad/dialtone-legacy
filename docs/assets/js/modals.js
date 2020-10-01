15c45888190f6d76fe7dd4fe17c2cd49a41fd82d
Joshua Hynes <hello@joshuahynes.com>
2020-10-01 16:25:10 -0400

    Start modal banner style

--- docs/assets/js/modals.js
+++ docs/assets/js/modals.js
@@ -1,7 +1,13 @@
 $(document).ready(function() {
     var body = $('body');
     var launchBtn = $('.js-launch-example-modal');
+    var launchFull = $('.js-launch-example-modal-full');
+    var launchBanner = $('.js-launch-example-modal-banner');
+    var launchBannerFull = $('.js-launch-example-modal-banner-full');
     var modal = $('.js-example-modal');
+    var modalFull = $('.js-example-modal-full');
+    var banner = $('.js-example-modal-banner');
+    var bannerFull = $('.js-example-modal-banner-full');
     var close = $('.js-example-modal-close');
 
     $(launchBtn).on('click', function(e) {
@@ -9,6 +15,29 @@ $(document).ready(function() {
         e.preventDefault();
 
         $(modal).attr('aria-hidden','false');
+        $(body).addClass('d-of-hidden');
+    });
+
+    $(launchFull).on('click', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        $(modalFull).attr('aria-hidden','false');
+        $(body).addClass('d-of-hidden');
+    });
+
+    $(launchBanner).on('click', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        $(banner).toggleClass('d-d-none');
+    });
+
+    $(launchBannerFull).on('click', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        $(bannerFull).toggleClass('d-d-none');
     });
 
     $(close).on('click', function(e) {
@@ -16,5 +45,9 @@ $(document).ready(function() {
         e.preventDefault();
 
         $(modal).attr('aria-hidden','true');
+        $(modalFull).attr('aria-hidden','true');
+        $(body).removeClass('d-of-hidden');
+        $(banner).removeClass('d-d-none').addClass('d-d-none');
+        $(bannerFull).removeClass('d-d-none').addClass('d-d-none');
     });
 });
