ddd6822b85c10bf2e1bb395618327e6bfcd429e5
Joshua Hynes <hello@joshuahynes.com>
2020-10-02 13:13:54 -0400

    More modal example functionality to clear modals using ESC key or if you click outside dialog window.

--- docs/assets/js/modals.js
+++ docs/assets/js/modals.js
@@ -5,10 +5,36 @@ $(document).ready(function() {
     var launchDanger = $('.js-launch-example-modal-danger');
     var launchBanner = $('.js-launch-example-modal-banner');
     var modal = $('.js-example-modal');
+    var dialog = $('.js-example-modal-dialog');
     var banner = $('.js-example-modal-banner');
     var btnSave = $('.js-example-modal-btn-save');
     var btnCancel = $('.js-example-modal-btn-cancel');
     var close = $('.js-example-modal-btn-cancel, .js-example-modal-close');
+
+    $.fn.closeModal = function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        $(modal).attr('aria-hidden','true');
+        if ($('.js-example-modal.d-modal--danger')) {
+            $(modal).removeClass('d-modal--danger');
+        }
+        if ($('.js-example-modal.d-modal--full')) {
+            window.setTimeout(function() {
+                $(modal).removeClass('d-modal--full');
+            }, 200);
+        }
+
+        $(body).removeClass('d-of-hidden');
+        $(banner).addClass('d-d-none');
+
+        if ($('.js-example-modal-btn-cancel.d-btn--danger')) {
+            $(btnCancel).removeClass('d-btn--danger').addClass('d-btn');
+        }
+        if ($('.js-example-modal-btn-save.d-btn--danger--primary')) {
+            $(btnSave).removeClass('d-btn--danger--primary').addClass('d-btn--primary');
+        }
+    };
 
     $(launchBtn).on('click', function(e) {
         e.stopPropagation();
@@ -43,28 +69,20 @@ $(document).ready(function() {
         $(banner).removeClass('d-d-none');
     });
 
+    $(document).on('keyup', function(e) {
+        if(e.keyCode === 27) {
+            $.fn.closeModal(e);
+        }
+    })
+    $(document).on('click', function(e) {
+        var target = $(e.target);
+
+        if(!target.closest(dialog).length && dialog.is(":visible")) {
+            $.fn.closeModal(e);
+        }
+    });
+
     $(close).on('click', function(e) {
-        e.stopPropagation();
-        e.preventDefault();
-
-        $(modal).attr('aria-hidden','true');
-        if ($('.js-example-modal.d-modal--danger')) {
-            $(modal).removeClass('d-modal--danger');
-        }
-        if ($('.js-example-modal.d-modal--full')) {
-            window.setTimeout(function() {
-                $(modal).removeClass('d-modal--full');
-            }, 200);
-        }
-
-        $(body).removeClass('d-of-hidden');
-        $(banner).addClass('d-d-none');
-
-        if ($('.js-example-modal-btn-cancel.d-btn--danger')) {
-            $(btnCancel).removeClass('d-btn--danger').addClass('d-btn');
-        }
-        if ($('.js-example-modal-btn-save.d-btn--danger--primary')) {
-            $(btnSave).removeClass('d-btn--danger--primary').addClass('d-btn--primary');
-        }
+        $.fn.closeModal(e);
     });
 });
