af79aa2fc6446e71fe2a734bf1a9aea7e07bb8b3
Drew Chandler <andrew.chandler@dialpad.com>
2021-04-16 10:55:03 -0600

    Removing unneeded jQuery additions.

--- docs/assets/js/modal.js
+++ docs/assets/js/modal.js
@@ -66,8 +66,6 @@ $(document).ready(function() {
         e.stopPropagation();
         e.preventDefault();
 
-        $(modal).attr('aria-hidden','false');
-        $(body).addClass('d-of-hidden');
         $(banner).removeClass('d-d-none');
     });
 
