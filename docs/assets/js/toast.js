56404bbc930235437588b0915433b86fe294bb53
Julio Ortega <julio.ortega@dialpad.com>
2021-08-26 21:02:24 -0500

    Added UC toast shims

--- docs/assets/js/toast.js
+++ docs/assets/js/toast.js
@@ -17,14 +17,16 @@ $(document).ready(function() {
   const actions = $('.js-toast-example-actions');
   const actionBtn = $('.js-toast-example-action-btn');
   const closeBtn = $('.js-toast-example-close-btn');
+  const meta = $('.js-toast-example-meta');
+  const message = $('.js-toast-example-message');
 
-  const iconTypes = $('.js-toast-example-icon-base, .js-toast-example-icon-info, .js-toast-example-icon-warning, .js-toast-example-icon-error, .js-toast-example-icon-success');
-  const styleClassTypes = ("d-toast--info d-toast--warning d-toast--error d-toast--success d-toast--important");
+  const iconTypes = $('.js-toast-example-icon-base, .js-toast-example-icon-info, .js-toast-example-icon-warning, .js-toast-example-icon-error, .js-toast-example-icon-success, .js-toast-example-icon-chat, .js-toast-example-icon-organizer, .js-toast-example-icon-viewing');
+  const styleClassTypes = ("d-toast--info d-toast--warning d-toast--error d-toast--success d-toast--important d-toast--chat d-toast--organizer d-toast--viewing");
 
 
   function closeToast() {
     const style = syleSelectMenu.find(':selected').data('class');
-    const iconStyle = `js-toast-example-icon-${style}`;
+    const iconStyle = $(`.js-toast-example-icon-${style}`);
 
     // Update controls
     launchBtn.text('Show example');
@@ -49,6 +51,34 @@ $(document).ready(function() {
 
     e.stopPropagation();
     e.preventDefault();
+
+    if(style === 'chat') {
+      meta.removeClass('d-d-none');
+    } else {
+      meta.addClass('d-d-none');
+    }
+
+    switch (style){
+      case 'chat':
+        message.text("Who's sharing their screen right now?");
+        break;
+      case 'organizer':
+        message.html('<span><b>Anonymous User</b> is now co-host.</span>');
+        break;
+      case 'viewing':
+        message.html('<span><b>Anonymous User</b> is viewing this call.</span>');
+        break;
+      default:
+        message.html('Message body with <a href="#" class="d-link d-link--muted js-toast-example-link">a link.</a>');
+    }
+
+    if(['chat', 'organizer', 'viewing'].includes(style)) {
+      title.addClass('d-d-none');
+      actions.addClass('d-d-none');
+    } else {
+      title.removeClass('d-d-none');
+      actions.removeClass('d-d-none');
+    }
 
     // Update controls
     $(this).text('Update example');
@@ -88,7 +118,7 @@ $(document).ready(function() {
     // Add toast style classes
     if (style != 'none') {
       toast.addClass(toastStyleClass)
-      actions.removeClass('d-d-none');
+      // actions.removeClass('d-d-none');
       icon.removeClass('d-d-none');
       iconStyle.removeClass('d-d-none');
     }
