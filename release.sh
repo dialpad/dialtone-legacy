5e50eaf1d73da27bbc1f2da8dc09a30ad69e9323
Brad Paugh <brad.paugh@dialpad.com>
2021-09-22 15:09:50 -0700

    better output in script

--- release.sh
+++ release.sh
@@ -2,7 +2,13 @@
 echo "Enter the version number of this release Ex/ 6.0.2 or 6.0.0-beta.16: "
 read version
 branch=$(git branch --show-current)
+
 echo "Committing new npm version and setting git tag"
-npm version $version
+npmcmd="npm version $version"
+echo "$npmcmd"
+$npmcmd
+
 echo "Pushing new commit and tag to remote"
-git push --atomic origin $branch $version
+pushcmd="git push --atomic origin $branch v$version"
+echo "$pushcmd"
+$pushcmd
