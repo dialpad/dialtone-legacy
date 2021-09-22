dfc95b0db7429101364f0ece9fbb99eaa4643895
Brad Paugh <brad.paugh@dialpad.com>
2021-09-22 16:09:45 -0700

    add set -e to release script

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# exit when any command fails
+set -e
+
 echo "Enter the version number of this release Ex/ 6.0.2 or 6.0.0-beta.16: "
 read version
 branch=$(git branch --show-current)
