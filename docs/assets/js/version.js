edfbd3bd635ca17fd93d7dd7ce18f5d20adfa912
Brad Paugh <brad.paugh@dialpad.com>
2021-09-28 13:39:44 -0700

    https

--- docs/assets/js/version.js
+++ docs/assets/js/version.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   const dialtoneVueBadge = $('#dialtone-vue-badge');
 
-  $.get('http://vue.dialpad.design/version.txt')
+  $.get('https://vue.dialpad.design/version.txt')
     .done(function(response) {
       const badgeSrc = `https://img.shields.io/badge/Vue-v${response}-A687FF`
       dialtoneVueBadge.attr("src", badgeSrc)
