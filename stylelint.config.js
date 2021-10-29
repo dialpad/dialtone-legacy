b34484cfc1b8b68202f2f2ea6a41a569e43a4773
nikitadialpad <81983630+nikitadialpad@users.noreply.github.com>
2021-10-29 15:35:59 +0300

    DT-242 Stylelint recommended (minimalistic)

---
+++ stylelint.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "extends": [
+    "stylelint-config-recommended",
+    "stylelint-config-rational-order",
+  ],
+  "customSyntax": "postcss-less",
+  "rules": {
+    "no-descending-specificity": null,
+    "function-calc-no-unspaced-operator": null
+  }
+};
