caec15bb5b7b2879d2896619eece00aced26687c
Brad Paugh <brad.paugh@dialpad.com>
2021-11-10 08:58:47 -0800

    Revert "fix: bad syntax in vue files when style tags are used in SVG (#498)"
    This reverts commit a64532ca29638626164b834eeebcbbcf31552fa8.

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,6 @@ var cache = require('gulp-cached');
 var order = require('gulp-order');
 var concat = require('gulp-concat');
 var remember = require('gulp-remember');
-var through2 = require('through2');
 var browsersync = require('browser-sync').create();
 var package = require('./package.json');
 
@@ -364,9 +363,6 @@ var buildSystemSVGs = function(done) {
         .pipe(dest(paths.svgs.sysOutputDocs))
         .pipe(replace('<svg', '<template>\n  <svg'))
         .pipe(replace('</svg>', '</svg>\n</template>'))
-        // move any style tags within the svg into style tags of the vue component
-        .pipe(through2.obj(moveStyleTagsToEOF))
-        .pipe(replace('<style>', '<style scoped>'))
         .pipe(rename(function(file) {
             var converted = file.basename.replace(/\b\S/g, t => t.toUpperCase()).replace(/[-]+/g, '');
 
@@ -377,20 +373,6 @@ var buildSystemSVGs = function(done) {
 
     done();
 };
-
-const moveStyleTagsToEOF = function(file, enc, cb) {
-    if (file.isBuffer()) {
-        const styleTagsRegex = /<style[\s\S]*<\/style>/gmi;
-        let code = file.contents.toString();
-        const result = styleTagsRegex.exec(code);
-        if (!result) return cb(null, file);
-        const matchedText = result[0];
-        code = code.replace(styleTagsRegex, '');
-        code = code + matchedText;
-        file.contents = Buffer.from(code)
-    }
-    return cb(null, file);
-}
 
 var buildBrandSVGs = function(done) {
 
@@ -432,11 +414,9 @@ var buildBrandSVGs = function(done) {
         .pipe(dest(paths.svgs.brandOutputDocs))
         .pipe(replace('<svg', '<template>\n  <svg'))
         .pipe(replace('</svg>', '</svg>\n</template>'))
-        // move any style tags within the svg into style tags of the vue component
-        .pipe(through2.obj(moveStyleTagsToEOF))
-        .pipe(replace('<style>', '<style scoped>'))
         .pipe(rename(function(file) {
-            const converted = file.basename.replace(/\b\S/g, t => t.toUpperCase()).replace(/[-]+/g, '');
+            var converted = file.basename.replace(/\b\S/g, t => t.toUpperCase()).replace(/[-]+/g, '');
+
             file.basename = 'Icon' + converted;
             file.extname = '.vue';
         }))
@@ -485,9 +465,6 @@ var buildPatternSVGs = function(done) {
         .pipe(dest(paths.patterns.outputDocs))
         .pipe(replace('<svg', '<template>\n  <svg'))
         .pipe(replace('</svg>', '</svg>\n</template>'))
-        // move any style tags within the svg into style tags of the vue component
-        .pipe(through2.obj(moveStyleTagsToEOF))
-        .pipe(replace('<style>', '<style scoped>'))
         .pipe(rename(function(file) {
             var converted = file.basename.replace(/\b\S/g, t => t.toUpperCase()).replace(/[-]+/g, '');
 
