6adc039819ec323890c2b8830f5b1e1fe2d0d53c
Brad Paugh <brad.paugh@dialpad.com>
2022-01-31 15:43:57 -0800

    Finish off documentation

--- docs/assets/js/popover.js
+++ docs/assets/js/popover.js
@@ -1,30 +1,56 @@
 $(document).ready(function() {
-  var popoverAnchor = $('.js-popover-anchor');
-  var body = document.body;
 
-  var shown = false;
-  $(popoverAnchor).on('click', function(e) {
-    const modal = $(e.target).closest('.dt-popover').siblings('.d-modal').first();
-    modal.addClass('d-modal--animate-in');
-    modal.removeClass('d-modal--animate-out');
-    const componentRoot = $(e.target).closest('.dt-popover');
-    const contentContainer = $(e.target).siblings('.dt-popover__content-container').first();
-    if (!shown) {
-      componentRoot.addClass('dt-popover--on-top');
-      contentContainer.removeClass('d-vi-hidden');
-      contentContainer.addClass('d-vi-visible');
-      modal.attr('aria-hidden','false');
+  const popoverAnchor = $('.js-popover-anchor');
+  const overlay = $('.d-modal');
+  const body = document.body;
+  let shown = false;
 
+  $.fn.closeModal = function() {
+    overlay.addClass('d-modal--animate-out');
+    overlay.removeClass('d-modal--animate-in');
+    $('.d-popover__dialog').removeClass('d-vi-visible');
+    $('.d-popover__dialog').addClass('d-vi-hidden');
+    overlay.attr('aria-hidden','true');
+    $(body).removeClass('d-of-hidden');
+    shown = !shown;
+  }
+
+  $.fn.openModal = function(e) {
+    const closestOverlay = $(e.target).closest('.d-popover').siblings('.d-modal').first();
+    const contentContainer = $(e.target).siblings('.d-popover__dialog').first();
+    closestOverlay.addClass('d-modal--animate-in');
+    closestOverlay.removeClass('d-modal--animate-out');
+    contentContainer.removeClass('d-vi-hidden');
+    contentContainer.addClass('d-vi-visible');
+    closestOverlay.attr('aria-hidden','false');
+    console.log(closestOverlay)
+
+    if (closestOverlay.length) {
       $(body).addClass('d-of-hidden');
-    } else {
-      modal.addClass('d-modal--animate-out');
-      modal.removeClass('d-modal--animate-in');
-      componentRoot.removeClass('dt-popover--on-top');
-      contentContainer.removeClass('d-vi-visible');
-      contentContainer.addClass('d-vi-hidden');
-      modal.attr('aria-hidden','true');
-      $(body).removeClass('d-of-hidden');
     }
     shown = !shown;
+  }
+
+  popoverAnchor.on('click', function(e) {
+    if (!shown) {
+      $.fn.openModal(e);
+    } else {
+      $.fn.closeModal();
+    }
+  });
+
+
+  $(document).on('keyup', function(e) {
+    if(e.keyCode === 27) {
+        $.fn.closeModal(e);
+    }
+  });
+
+  overlay.on('click', function(e) {
+    var target = $(e.target);
+
+    if(!target.closest('.d-popover__dialog').length && $('.d-popover__dialog').is(":visible")) {
+        $.fn.closeModal(e);
+    }
   });
 });
