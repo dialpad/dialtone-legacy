5276dce4001e872a9750723321986289f42cd7dc
juliodialpad <julio.ortega@dialpad.com>
2022-04-12 17:23:50 -0500

    add dark prop to CodeWellHeader

--- docs/.vuepress/components/CodeWellHeader.vue
+++ docs/.vuepress/components/CodeWellHeader.vue
@@ -1,6 +1,9 @@
 <template>
   <aside class="d-mt16 d-btr8 d-of-hidden">
-    <header class="d-fl-center d-fd-column d-p24 d-bgc-black-025 d-of-auto d-stack8">
+    <header
+      class="d-fl-center d-fd-column d-p24 d-of-auto d-stack8"
+      :class="dark ? 'd-bgc-black-800' : 'd-bgc-black-025'"
+    >
       <slot></slot>
     </header>
   </aside>
@@ -9,5 +12,11 @@
 <script>
 export default {
   name: "CodeWellHeader",
+  props: {
+    dark: {
+      type: Boolean,
+      default: false,
+    },
+  },
 }
 </script>
