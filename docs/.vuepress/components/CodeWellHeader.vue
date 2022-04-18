044e4919c459472709c3440043777bfbec742446
juliodialpad <julio.ortega@dialpad.com>
2022-04-18 12:26:36 -0500

    add border when background is white

--- docs/.vuepress/components/CodeWellHeader.vue
+++ docs/.vuepress/components/CodeWellHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <aside class="d-mt16 d-btr8 d-of-hidden">
     <header
-      class="d-fl-center d-fd-column d-p24 d-of-auto d-stack8"
-      :class="bgclass"
+      class="d-fl-center d-fd-column d-p24 d-w100p d-of-auto d-stack8"
+      :class="customClasses"
     >
       <slot></slot>
     </header>
@@ -19,5 +19,15 @@ export default {
       default: 'd-bgc-black-025',
     },
   },
+  computed: {
+    customClasses() {
+      return [
+        this.bgclass,
+        {
+          'd-ba d-bc-black-050 d-btr8 d-baw2': this.bgclass === 'd-bgc-white'
+        }
+      ];
+    }
+  }
 }
 </script>
