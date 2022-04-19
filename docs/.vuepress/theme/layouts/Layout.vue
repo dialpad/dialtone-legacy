9c4d7caa050ef07b93f83c237ca6f5d39b905747
juliodialpad <julio.ortega@dialpad.com>
2022-04-19 12:13:11 -0500

    fix storybook links

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -14,11 +14,11 @@ export default {
   <ParentLayout>
     <template #page-top>
       <header class="theme-default-content d-stack2">
-        <h1 v-if="$page.frontmatter.title" class="d-headline42 d-lh6 d-mb4 d-pt16">{{ $page.frontmatter.title }}</h1>
+        <h1 v-if="$page.frontmatter.title" class="d-headline42 d-lh6 d-mb4 d-pt16" v-text="$page.frontmatter.title" />
         <a v-if="$page.frontmatter.storybook_url" class="d-link d-fs14 d-mb24"
-           href="{{ $page.frontmatter.storybook_url }}" target="_blank">
+           :href="$page.frontmatter.storybook_url" target="_blank">
           View Storybook
-          <icon-launch></icon-launch>
+          <icon-launch />
         </a>
         <p v-if="$page.frontmatter.desc" class="d-fs20 d-lh6" v-html="$page.frontmatter.desc"></p>
       </header>
