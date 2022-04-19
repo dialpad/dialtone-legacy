f319fd8d13683f5937926e29725d563a49523508
juliodialpad <87546543+juliodialpad@users.noreply.github.com>
2022-04-19 01:09:17 +0000

    Deploying to gh-pages from @ dialpad/dialtone@2f38c75b780f163d391f54a45a94ed06413c6a1f 🚀

---
+++ vuepress/assets/IconDelete.499e3d07.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as t,a}from"./app.213c74f7.js";const c={},o={"aria-hidden":"true","aria-label":"Delete",class:"d-svg d-svg--system d-svg__delete",viewBox:"0 0 24 24"},_=a("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"},null,-1),r=[_];function n(d,l){return s(),t("svg",o,r)}var i=e(c,[["render",n]]);export{i as default};
---
+++ vuepress/assets/index.html.31b0b5da.js
@@ -0,0 +1,1 @@
+const e={key:"v-8daa1a0e",path:"/",title:"",lang:"en-US",frontmatter:{home:!0,layout:"Home"},excerpt:"",headers:[],git:{updatedTime:1650330434e3,contributors:[{name:"juliodialpad",email:"julio.ortega@dialpad.com",commits:1}]},filePathRelative:"index.md"};export{e as data};
