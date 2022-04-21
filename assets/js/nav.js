6afb848798f428d438557e3fd6a1b90b48ce9c68
braddialpad <brad.paugh@dialpad.com>
2022-04-21 20:14:07 +0000

    Deploying to gh-pages from @ dialpad/dialtone@f920b7889c4a4207151bc434dacd29a3d840c595 🚀

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,12 +5,20 @@ $(document).ready(function() {
     var body = $("body");
     var navHeader = $(".js-navigation-header");
     var navigationSideBar = $(".js-navigation-sidebar");
+    var search = $(".js-dialtone-search")
     var banner = $('.js-banner-example');
     const dropDownNavigation = $('.js-mobile-header-drop-down-navigation')
     const navigationHeader = $('.js-navigation-header')
     const breadcrumbsWrapper = $('.js-mobile-header-breadcrumbs')
     const breadcrumbArrow = $('.js-mobile-header-breadcrumb-arrow')
 
+    convertSearchToDialtone();
+
+    // add dialtone child elements to the algolia search button (label and icon)
+    function convertSearchToDialtone () {
+      $('<span class="d-btn__label">Search Dialtone</span>').appendTo(search.find('button'))
+      $('<span class="d-btn__icon d-btn__icon--left"><svg aria-hidden="true" aria-label="Search" class="d-svg d-svg--system d-svg__search" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M15.732 14.318h-.741l-.263-.253a6.095 6.095 0 001.389-5.009c-.441-2.607-2.619-4.69-5.246-5.009a6.103 6.103 0 00-6.823 6.82c.319 2.626 2.402 4.802 5.011 5.243a6.103 6.103 0 005.012-1.388l.253.262v.741l3.989 3.987a.992.992 0 001.398 0 .99.99 0 000-1.398l-3.979-3.996zm-5.631 0a4.217 4.217 0 01-4.223-4.22A4.216 4.216 0 0110.1 5.876a4.216 4.216 0 014.223 4.22 4.216 4.216 0 01-4.223 4.221z"></path></svg></span>').appendTo(search.find('button'))
+    }
 
     function toggleDropDownNavigation () {
         breadcrumbArrow.toggleClass('breadcrumb-arrow--top')
