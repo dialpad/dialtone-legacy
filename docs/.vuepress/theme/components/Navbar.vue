94d0f5d253a32dcc183ceba668f14b317d3bde16
juliodialpad <julio.ortega@dialpad.com>
2022-04-26 12:44:14 -0500

    add dialtone styling to search box

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -40,7 +40,7 @@
             </div>
         </a> -->
       </div>
-      <div id="algolia-search-container" />
+      <div id="algolia-search-container" ref="searchBtn" />
     </div>
   </header>
 </template>
@@ -73,7 +73,25 @@ export default {
           appId: 'Y5HG9UX6KM',
           placeholder: 'Search Dialtone',
         })
+
+        this.convertSearchToDialtone();
       })
+    },
+    convertSearchToDialtone () {
+      const DocSearchBtn = this.$refs.searchBtn.children.item(0);
+      const searchString = document.createElement('span');
+      searchString.className = 'd-btn__label';
+      searchString.textContent = 'Search Dialtone'
+
+      const searchIcon = document.createElement('span');
+      searchIcon.className = 'd-btn__icon d-btn__icon--left';
+      searchIcon.innerHTML = '' +
+          '<svg aria-hidden="true" aria-label="Search" class="d-svg d-svg--system d-svg__search" viewBox="0 0 24 24">\n' +
+            '<path clip-rule="evenodd" d="M15.732 14.318h-.741l-.263-.253a6.095 6.095 0 001.389-5.009c-.441-2.607-2.619-4.69-5.246-5.009a6.103 6.103 0 00-6.823 6.82c.319 2.626 2.402 4.802 5.011 5.243a6.103 6.103 0 005.012-1.388l.253.262v.741l3.989 3.987a.992.992 0 001.398 0 .99.99 0 000-1.398l-3.979-3.996zm-5.631 0a4.217 4.217 0 01-4.223-4.22A4.216 4.216 0 0110.1 5.876a4.216 4.216 0 014.223 4.22 4.216 4.216 0 01-4.223 4.221z" fill-rule="evenodd" />\n' +
+          '</svg>'
+
+      DocSearchBtn.appendChild(searchString);
+      DocSearchBtn.appendChild(searchIcon);
     },
   }
 }
