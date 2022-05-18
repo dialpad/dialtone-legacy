859c9c9754ce78ecd9320d1fc7594d47f2b85c4a
Brad Paugh <brad.paugh@dialpad.com>
2022-05-17 17:20:00 -0700

    add active close logic (#594)
    * add active close logic
    
    * remove comment

--- docs/assets/js/chip.js
+++ docs/assets/js/chip.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
-    var closeButton = $('.js-chip-button');
+    var chip = $('.js-chip');
+    var chipBtn = $('.js-chip-button');
 
-    $(closeButton).on('click', function(e) {
-        console.log('clicked');
-        e.stopPropagation();
-        e.preventDefault();
+    $(chip).on('mousedown', function(e) {
+        const target = $(e.target);
+        if (!target.parents().is(chipBtn)) {
+            chip.addClass('d-chip--active');
+        }
+    });
+
+    $(document).on('mouseup', function(e) {
+        chip.removeClass('d-chip--active');
     });
 });
