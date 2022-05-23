e83af72dc57a24e8e1758e240053cca7feda2b13
juliodialpad <juliodialpad@users.noreply.github.com>
2022-05-23 17:22:19 +0000

    Deploying to gh-pages from @ dialpad/dialtone@dfd1b52786757b3142b69a2d2844860c84e05af0 🚀

---
+++ deploy-previews/pr-598/assets/color-palette.html.225c1f0f.js
@@ -0,0 +1,1 @@
+import{_ as e,r,o as t,x as c}from"./app.4cea19cb.js";const _={};function l(n,s){const o=r("colors");return t(),c(o)}var f=e(_,[["render",l],["__file","color-palette.html.vue"]]);export{f as default};
