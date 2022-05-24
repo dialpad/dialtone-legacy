cb48a7d1dff8744d0dca72ff6030d5cf3a135f5b
juliodialpad <87546543+juliodialpad@users.noreply.github.com>
2022-05-24 17:57:41 +0000

    Deploying to gh-pages from @ dialpad/dialtone@4ced53ecd4a8b4074ae27e915645f0111e693efa 🚀

---
+++ independent-deploy/assets/404.html.0f58907d.js
@@ -0,0 +1,1 @@
+import{_}from"./app.95b9f7c4.js";const e={};function r(t,c){return null}var f=_(e,[["render",r],["__file","404.html.vue"]]);export{f as default};
---
+++ independent-deploy/assets/brand.html.a00a147d.js
@@ -0,0 +1,1 @@
+import{_ as e,r as o,o as r,x as c}from"./app.95b9f7c4.js";const t={};function _(a,s){const n=o("icons");return r(),c(n,{kind:"brand"})}var f=e(t,[["render",_],["__file","brand.html.vue"]]);export{f as default};
