61bfa7ba6fe8a2b9722ab8f113525195237c00a7
juliodialpad <juliodialpad@users.noreply.github.com>
2022-05-24 17:33:05 +0000

    Deploying to gh-pages from @ dialpad/dialtone@7c49abf76a37b735ac325ef535932c9e60d620fd 🚀

--- deploy-previews/pr-599/assets/brand.html.dc7104ca.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,r as o,o as r,x as c}from"./app.a573a3ac.js";const t={};function _(a,s){const n=o("icons");return r(),c(n,{kind:"brand"})}var f=e(t,[["render",_],["__file","brand.html.vue"]]);export{f as default};
---
+++ deploy-previews/pr-599/assets/weather.html.5e5df521.js
@@ -0,0 +1,1 @@
+import{_ as o,r as a,o as r,c,b as e,F as s}from"./app.85119c6b.js";const _={};function i(l,h){const t=a("icons"),n=a("weather-codes-table");return r(),c(s,null,[e(t,{kind:"weather",title:"Day Icons",variation:"day"}),e(t,{kind:"weather",title:"Night Icons",variation:"night"}),e(n)],64)}var d=o(_,[["render",i],["__file","weather.html.vue"]]);export{d as default};
