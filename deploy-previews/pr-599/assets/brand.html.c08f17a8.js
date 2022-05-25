cf36a45d43624f8d0e7d5a77f757bc4cdcee6c45
juliodialpad <juliodialpad@users.noreply.github.com>
2022-05-25 22:46:03 +0000

    Deploying to gh-pages from @ dialpad/dialtone@e6440ce1063740767d38460d8cb671ce11e960f7 🚀

--- deploy-previews/pr-599/assets/brand.html.c08f17a8.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,r as o,o as r,x as c}from"./app.88aa4d43.js";const t={};function _(a,s){const n=o("icons");return r(),c(n,{kind:"brand"})}var f=e(t,[["render",_],["__file","brand.html.vue"]]);export{f as default};
