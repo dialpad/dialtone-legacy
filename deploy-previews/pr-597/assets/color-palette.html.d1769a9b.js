af0912a6c60f14023e196bf54f1cab5affa044de
DestaDialpad <DestaDialpad@users.noreply.github.com>
2022-05-26 00:03:35 +0000

    Deploying to gh-pages from @ dialpad/dialtone@ffc55e311db340a23a6cb26b4cfbb5e4340c9dba 🚀

---
+++ deploy-previews/pr-597/assets/color-palette.html.d1769a9b.js
@@ -0,0 +1,1 @@
+import{_ as e,r,o as t,y as c}from"./app.f6e27927.js";const _={};function l(n,s){const o=r("colors");return t(),c(o)}var f=e(_,[["render",l],["__file","color-palette.html.vue"]]);export{f as default};
