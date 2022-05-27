7e8941c316ca9be558ad20a31ce1e79bf114e283
francisrupert <francisrupert@users.noreply.github.com>
2022-05-27 01:40:26 +0000

    Deploying to gh-pages from @ dialpad/dialtone@788982ac10e63bef755e66a00c09079f9274a185 🚀

---
+++ deploy-previews/pr-601/assets/brand.html.c4665765.js
@@ -0,0 +1,1 @@
+import{_ as e,r as o,o as r,y as c}from"./app.58f14a12.js";const t={};function _(a,s){const n=o("icons");return r(),c(n,{kind:"brand"})}var f=e(t,[["render",_],["__file","brand.html.vue"]]);export{f as default};
