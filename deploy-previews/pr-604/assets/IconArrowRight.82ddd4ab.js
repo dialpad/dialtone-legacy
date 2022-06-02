0ff25690bdaea5525c08d14bbaaafd23d4fef1e7
josedialpad <josedialpad@users.noreply.github.com>
2022-06-02 18:05:55 +0000

    Deploying to gh-pages from @ dialpad/dialtone@08e1dfcc4960620a2ec9635b661108f0fc9385e0 🚀

--- deploy-previews/pr-604/assets/IconArrowRight.82ddd4ab.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as r,a as s}from"./app.db970c53.js";const t={},a={"aria-hidden":"true","aria-label":"Arrow Right",class:"d-svg d-svg--system d-svg__arrowRight",viewBox:"0 0 24 24"},c=s("path",{d:"M9 7l5 5-5 5V7z"},null,-1),_=[c];function n(i,d){return o(),r("svg",a,_)}var h=e(t,[["render",n],["__file","IconArrowRight.vue"]]);export{h as default};
