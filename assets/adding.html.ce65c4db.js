72e5577a8afed9a308307081821e8d464020e2a4
josedialpad <jose.silva@dialpad.com>
2022-06-03 20:01:11 +0000

    Deploying to gh-pages from @ dialpad/dialtone@f73086cc1845cf29fcdea46ba871e80767a4ac21 🚀

--- assets/adding.html.ce65c4db.js
+++
@@ -1,1 +0,0 @@
-const t={key:"v-3bded04b",path:"/design/icons/adding.html",title:"Adding Icons",lang:"en-US",frontmatter:{title:"Adding Icons",next:{text:"Patterns",link:"/design/illustrations/patterns"}},excerpt:"",headers:[],git:{updatedTime:1653607536e3},filePathRelative:"design/icons/adding.md"};export{t as data};
