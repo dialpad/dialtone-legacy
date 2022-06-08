83fd9a7d03bb5579c3af4fc1e3507d80b8175405
francisrupert <francisrupert@users.noreply.github.com>
2022-06-08 16:03:33 +0000

    Deploying to gh-pages from @ dialpad/dialtone@9fad0552b465dcd2c226f490537635453df27249 🚀

--- deploy-previews/pr-607/assets/color-palette.html.3582582d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,r,o as t,y as c}from"./app.0bd654b2.js";const _={};function l(n,s){const o=r("colors");return t(),c(o)}var f=e(_,[["render",l],["__file","color-palette.html.vue"]]);export{f as default};
