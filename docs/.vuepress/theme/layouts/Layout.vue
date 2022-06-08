209af2909f87d3caba7b5eb864327fe3c1d44e5c
Francis Rupert <francisrupert@gmail.com>
2022-06-08 11:36:55 -0500

    docs(style): basic polish of pagination

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -8,7 +8,7 @@
     <template #page-top>
       <section class="theme-default-content d-stack32 d-pb0 d-mb8">
         <header class="d-stack2">
-          <h1 v-if="$page.frontmatter.title" class="d-headline42 d-lh6 d-mb4 d-pt16" v-text="$page.frontmatter.title" />
+          <h1 v-if="$page.frontmatter.title" class="d-headline42 d-lh6 d-mb8 d-pt16" v-text="$page.frontmatter.title" />
           <p v-if="$page.frontmatter.desc" class="d-fs20 d-lh6" v-html="$page.frontmatter.desc"></p>
         </header>
         <div v-if="$page.path.startsWith('/components')" class="d-d-flex d-jc-space-between">
