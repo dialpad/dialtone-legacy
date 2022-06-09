d28ff108d15958071df8b720ba16bacc6bf91b93
francisrupert <francisrupert@users.noreply.github.com>
2022-06-09 12:47:26 +0000

    Deploying to gh-pages from @ dialpad/dialtone@8a6694250dad380295afb27da8440814c4963b03 🚀

--- deploy-previews/pr-607/assets/IconNotes.8fa517e2.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as o,a as t}from"./app.f8837885.js";const a={},c={"aria-hidden":"true","aria-label":"Notes",class:"d-svg d-svg--system d-svg__notes",viewBox:"0 0 24 24"},_=t("path",{d:"M3 18h12v-2H3v2zM3 6v2h18V6H3zm0 7h18v-2H3v2z"},null,-1),n=[_];function r(d,v){return s(),o("svg",c,n)}var l=e(a,[["render",r],["__file","IconNotes.vue"]]);export{l as default};
