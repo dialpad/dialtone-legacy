a514ba5300e73b63ae298918eacb509185d5e1ee
Julio Ortega <87546543+juliodialpad@users.noreply.github.com>
2022-06-10 14:29:38 -0500

    fix(tabs): keypress events not working (#609)
    * fix(tabs): keypress events not working
    
    * docs(tabs): cleanup examples
    
    * fix enter event not working well
    change keyup -> keydown

--- docs/.vuepress/exampleComponents/ExampleTabs.vue
+++ docs/.vuepress/exampleComponents/ExampleTabs.vue
@@ -8,78 +8,115 @@
         'd-tablist--inverted': inverted
       }"
       role="tablist"
+      ref="tabs"
       aria-label="Label Example Group"
+      @keydown.left="tabLeft"
+      @keydown.right="tabRight"
+      @keydown.enter="selectTab(focusId)"
+      @keydown.space="selectTab(focusId)"
+      @keydown.home.prevent="onHomeButton"
+      @keydown.end.prevent="onEndButton"
     >
       <button
+        v-for="(name, index) in tabNames"
         class="d-tab"
-        :class="{'d-tab--selected': selected === 0}"
+        :class="{'d-tab--selected': selected === index}"
         role="tab"
-        :aria-selected="selected === 0"
-        aria-controls="base-panel-1"
-        id="base-tab-1"
-        tabindex="0"
-        @click="selectTab(0)"
+        :aria-selected="selected === index"
+        :aria-controls="`base-panel-${index}`"
+        :id="`base-tab-${index}`"
+        :tabindex="selected === index ? 0 : -1"
+        @click="selectTab(index)"
       >
-        First tab
-      </button>
-      <button
-        class="d-tab"
-        :class="{'d-tab--selected': selected === 1}"
-        role="tab"
-        :aria-selected="selected === 1"
-        aria-controls="base-panel-2"
-        id="base-tab-2"
-        tabindex="-1"
-        @click="selectTab(1)"
-      >
-        Second tab
-      </button>
-      <button
-        class="d-tab"
-        :class="{'d-tab--selected': selected === 2}"
-        role="tab"
-        :aria-selected="selected === 2"
-        aria-controls="base-panel-3"
-        id="base-tab-3"
-        tabindex="-1"
-        @click="selectTab(2)"
-      >
-        Third tab
+        {{ name }} tab
       </button>
     </div>
-    <slot name="panel_content" :selected="selected"></slot>
+    <div v-if="!hideContent">
+      <div
+        v-for="(name, index) in tabNames"
+        :id="`base-panel-${index}`"
+        role="tabpanel"
+        tabindex="0"
+        :aria-labelledby="`base-tab-${index}`"
+        :hidden="selected !== index"
+      >
+        <p :class="{ 'd-fc-white': inverted }">{{ name }} tab content panel</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "example-tabs",
+
   data() {
     return {
-      selected: 0
+      focusId: null,
+      selected: 0,
+      tabNames: ['First', 'Second', 'Third'],
+      tabs: [],
     };
   },
+
   props: {
     size: {
       type: String,
+      default: '',
     },
+
     borderless: {
       type: Boolean,
       default: false,
     },
+
     inverted: {
       type: Boolean,
       default: false,
     },
-  },
-  methods: {
-    selectTab(tabNumber) {
-      this.selected = tabNumber;
+
+    hideContent: {
+      type: Boolean,
+      default: false,
     }
+  },
+
+  mounted() {
+    this.tabs = this.getTabChildren();
+  },
+
+  methods: {
+    getTabChildren () {
+      const tabs = Array.from(this.$refs.tabs.children);
+      return tabs.map(el =>  ({ context: el }));
+    },
+
+    focusTab(tabIndex) {
+      this.focusId = tabIndex;
+      this.tabs[tabIndex].context.focus();
+    },
+
+    selectTab(tabIndex) {
+      this.selected = tabIndex;
+    },
+
+    tabLeft () {
+      if (this.focusId === 0) return;
+      this.focusTab(this.focusId - 1)
+    },
+
+    tabRight () {
+      if (this.focusId === this.tabs.length - 1) return;
+      this.focusTab(this.focusId + 1)
+    },
+
+    onHomeButton () {
+      this.focusTab(0)
+    },
+
+    onEndButton () {
+      this.focusTab(this.tabs.length - 1)
+    },
   }
 }
 </script>
-
-<style scoped>
-
-</style>
