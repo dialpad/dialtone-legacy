c81f95de5c23c491cf42a0333db11bcb12ab7b5e
jonathanpaugh <jonathan.paugh@dialpad.com>
2022-06-10 16:04:00 -0700

    docs(toc): add responsive positioning

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,13 +1,13 @@
 <template>
   <ParentLayout>
     <template #page>
-      <div class="d-d-flex d-jc-space-between">
-        <Page class="d-fl-grow1">
+      <div class="container">
+        <Page>
           <template #top>
             <PageHeader :page="$page" />
           </template>
         </Page>
-        <PageToc class="d-pr16 d-pt16" :headers="$page.headers" />
+        <PageToc class="d-pr16" :headers="$page.headers" />
       </div>
     </template>
   </ParentLayout>
@@ -19,3 +19,40 @@
   import PageToc from "@theme/PageToc.vue";
   import PageHeader from "@theme/PageHeader.vue";
 </script>
+
+<style lang="less">
+  .container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+  }
+
+  .page {
+    flex-basis: 0;
+    flex-grow: 999;
+    min-inline-size: 50%;
+    width: 100%;
+  }
+
+  .toc {
+    top: var(--su64);
+    padding-top: var(--su16);
+    position: sticky;
+    flex-grow: 1;
+    align-self: flex-start;
+  }
+
+  @media screen and (max-width: 980px) {
+    .container {
+      flex-direction: column-reverse;
+      width: 100%;
+    }
+
+    .toc {
+      top: var(--su128);
+      background-color: white;
+      width: 100%;
+      z-index: 10;
+    }
+  }
+</style>
