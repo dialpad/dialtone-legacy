d5ea6953be064469a184d665a5bf1e5554a90205
juliodialpad <julio.ortega@dialpad.com>
2022-06-13 18:17:44 -0500

    docs: fix styling issues.

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -6,13 +6,13 @@
 <template>
   <ParentLayout>
     <template #page-top>
-      <section class="theme-default-content d-stack32 d-pt64 d-pb0 d-mb8">
+      <section class="theme-default-content d-stack32 d-pb0 d-mb8">
         <header class="d-stack2">
           <h1 v-if="$page.frontmatter.title" class="d-headline42 d-lh6 d-mb8 d-pt16" v-text="$page.frontmatter.title" />
           <p v-if="$page.frontmatter.desc" class="d-fs20 d-lh6" v-html="$page.frontmatter.desc"></p>
         </header>
-        <div v-if="$page.path.startsWith('/components')" class="d-d-flex d-jc-space-between">
-          <h2 class="d-fs16">Preview</h2>
+        <div id="preview-header" v-if="$page.path.startsWith('/components')" class="d-d-flex d-jc-space-between">
+          <h2 class="d-fs16 d-ba-unset">Preview</h2>
           <a v-if="$page.frontmatter.storybook_url" class="d-link d-fs14"
              :href="$page.frontmatter.storybook_url" target="_blank">
             <span class="d-mr4">View Storybook</span>
