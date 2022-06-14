423fe82b1d6a98fa7368621aee41ba3c8b18247b
jonathanpaugh <jonathan.paugh@dialpad.com>
2022-06-14 14:27:08 -0700

    docs: fix page header merge

--- docs/.vuepress/theme/components/PageHeader.vue
+++ docs/.vuepress/theme/components/PageHeader.vue
@@ -6,8 +6,8 @@
       <p v-if="frontmatter.desc" class="d-fs20 d-lh6" v-html="frontmatter.desc"></p>
     </header>
     <slot name="content-bottom" />
-    <div v-if="path.startsWith('/components')" class="d-d-flex d-jc-space-between">
-      <h2 class="d-fs16">Preview</h2>
+    <div id="preview-header" v-if="path.startsWith('/components')" class="d-d-flex d-jc-space-between">
+      <h2 class="d-fs16 d-ba-unset">Preview</h2>
       <a v-if="frontmatter.storybook_url" class="d-link d-fs14"
          :href="frontmatter.storybook_url" target="_blank">
         <span class="d-mr4">View Storybook</span>
