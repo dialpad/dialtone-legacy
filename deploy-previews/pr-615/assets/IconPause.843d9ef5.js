60b9656d08fc3d35466bde3f7de791b2640f1232
JonathanPaugh <JonathanPaugh@users.noreply.github.com>
2022-06-14 20:44:36 +0000

    Deploying to gh-pages from @ dialpad/dialtone@edb58e35a94baf85ea5b97fbb4751b7769441a7e 🚀

--- deploy-previews/pr-615/assets/IconPause.843d9ef5.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as a,a as o}from"./app.77ded562.js";const t={},c={"aria-hidden":"true","aria-label":"Pause",class:"d-svg d-svg--system d-svg__pause",viewBox:"0 0 24 24"},_=o("path",{d:"M6 19h4V5H6v14zm8-14v14h4V5h-4z"},null,-1),n=[_];function r(d,i){return s(),a("svg",c,n)}var u=e(t,[["render",r],["__file","IconPause.vue"]]);export{u as default};
