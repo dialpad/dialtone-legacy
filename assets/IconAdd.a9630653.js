f892b4143385a8baab2561f4d9f433907d382914
JonathanPaugh <j.d.paugh@gmail.com>
2022-06-14 23:00:19 +0000

    Deploying to gh-pages from @ dialpad/dialtone@a18743544dcdd88b705d049c003c0cef9aa04ecd 🚀

---
+++ assets/IconAdd.a9630653.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as a,a as d}from"./app.db519139.js";const o={},t={"aria-hidden":"true","aria-label":"Add",class:"d-svg d-svg--system d-svg__add",viewBox:"0 0 24 24"},c=d("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"},null,-1),_=[c];function n(r,v){return s(),a("svg",t,_)}var i=e(o,[["render",n],["__file","IconAdd.vue"]]);export{i as default};
