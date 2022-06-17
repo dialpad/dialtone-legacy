671a6c34b33284f673f74c98f9b71a6060bfe98c
josedialpad <jose.silva@dialpad.com>
2022-06-17 20:01:04 +0000

    Deploying to gh-pages from @ dialpad/dialtone@8851e1458f1ed1ee7972dc150d30a4be2c6a045b 🚀

--- assets/IconPlay.be42ca9d.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c as s,a as o}from"./app.db519139.js";const t={},c={"aria-hidden":"true","aria-label":"Play",class:"d-svg d-svg--system d-svg__play",viewBox:"0 0 24 24"},_=o("path",{d:"M8 5v14l11-7L8 5z"},null,-1),n=[_];function r(l,d){return a(),s("svg",c,n)}var v=e(t,[["render",r],["__file","IconPlay.vue"]]);export{v as default};
