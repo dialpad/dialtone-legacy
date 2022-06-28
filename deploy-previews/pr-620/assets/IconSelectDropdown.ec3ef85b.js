fb4707d9617848854ce2c7d8482595d7f67969c6
juliodialpad <juliodialpad@users.noreply.github.com>
2022-06-28 20:10:20 +0000

    Deploying to gh-pages from @ dialpad/dialtone@084651fc82b03076b3e44b9a2c75ad8d4a04e6c5 🚀

---
+++ deploy-previews/pr-620/assets/IconSelectDropdown.ec3ef85b.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as t}from"./app.3babaef5.js";const c={},a={"aria-hidden":"true","aria-label":"Select Dropdown",class:"d-svg d-svg--system d-svg__selectDropdown",viewBox:"0 0 24 24"},n=t("path",{d:"M7 13l5 5 5-5H7zM7 11l5-5 5 5H7z"},null,-1),r=[n];function _(d,l){return o(),s("svg",a,r)}var p=e(c,[["render",_],["__file","IconSelectDropdown.vue"]]);export{p as default};
