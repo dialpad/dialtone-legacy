02b0e7708e34232c158cb192039fc6146a4665a6
Jonathan Paugh <jonathan.paugh@dialpad.com>
2022-07-04 09:03:46 -0700

    docs: fix anchor scroll position (#623)
    * docs: fix anchor scroll positions
    
    * docs: modify active header link offset
    
    * docs: remove console log
    
    * docs: add router hashchange listener
    
    * Revert "docs: add router hashchange listener"
    
    This reverts commit 8c3865cd3218b94a596cd9a68ffdc9b4a6ac30c4.
    
    * Revert "docs: remove console log"
    
    This reverts commit ad11ef018416f13b7e9afd65973cd7458487bc91.
    
    * Revert "docs: modify active header link offset"
    
    This reverts commit ac7c9e7e2b847ac95d5ca54e54523d4d673d459f.
    
    * Revert "docs: fix anchor scroll positions"
    
    This reverts commit 8cb605abdb9547279ef50e01ba2493eda67c4e01.
    
    * docs: fix anchor scroll positions
    
    * docs: add `immediate` to navbar height watcher

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -32,9 +32,15 @@
   import PageToc from '@theme/PageToc.vue';
   import PageHeader from '@theme/PageHeader.vue';
 
-  import {useScrollPromise} from '@vuepress/theme-default/lib/client/composables'
-  import {ref} from 'vue';
+  import { useScrollPromise } from '@vuepress/theme-default/lib/client/composables'
+  import { ref, watch } from 'vue';
 
+  const scrollPromise = useScrollPromise();
+  const onBeforeEnter = scrollPromise.resolve;
+  const onBeforeLeave = scrollPromise.pending;
+
+  const defaultHeaderSize = 64;
+  const mobileHeaderSize = 128;
   const mobileBreakpoint = 980;
   const evaluateWindowWidth = () => window.innerWidth <= mobileBreakpoint;
 
@@ -43,9 +49,13 @@
     isMobile.value = evaluateWindowWidth();
   });
 
-  const scrollPromise = useScrollPromise();
-  const onBeforeEnter = scrollPromise.resolve;
-  const onBeforeLeave = scrollPromise.pending;
+  watch(isMobile, (current) => {
+    const root = document.querySelector(':root');
+    root.style.setProperty(
+      '--navbar-height',
+      `${(current ? mobileHeaderSize : defaultHeaderSize)}px`
+    );
+  }, { immediate: true });
 </script>
 
 <style lang="less">
