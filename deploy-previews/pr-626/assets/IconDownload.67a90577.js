49e9e252acc350714ff78ee1aaa22effc4aad32d
arjundialpad <arjundialpad@users.noreply.github.com>
2022-07-15 08:02:08 +0000

    Deploying to gh-pages from @ dialpad/dialtone@b896e60865f6a9ffb404961698d745f473890cc6 🚀

--- deploy-previews/pr-626/assets/IconDownload.67a90577.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o,c as a,a as s}from"./app.d4aee73a.js";const n={},t={"aria-hidden":"true","aria-label":"Download",class:"d-svg d-svg--system d-svg__download",viewBox:"0 0 24 24"},c=s("path",{d:"M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"},null,-1),_=[c];function d(r,l){return o(),a("svg",t,_)}var v=e(n,[["render",d],["__file","IconDownload.vue"]]);export{v as default};
---
+++ deploy-previews/pr-626/assets/patterns.html.404554b2.js
@@ -0,0 +1,1 @@
+import{_ as n,r as a,o as r,c as s,b as e,e as o}from"./app.40ee89be.js";const c={},i=o();function _(l,d){const t=a("icons");return r(),s("div",null,[e(t,{kind:"patterns",variation:"dark",title:"Dark Patterns"}),i,e(t,{kind:"patterns",variation:"light",class:"d-mt64",title:"Light Patterns"})])}var m=n(c,[["render",_],["__file","patterns.html.vue"]]);export{m as default};
