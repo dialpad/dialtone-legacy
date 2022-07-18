ec01fb9d79f08dd31a3bdebfc4cfd8f84a07bb08
josedialpad <jose.silva@dialpad.com>
2022-07-18 12:08:13 +0000

    Deploying to gh-pages from @ dialpad/dialtone@d01e3ccaf367b25774ecfa693919c7fea44b826c 🚀

---
+++ assets/IconMinimize.c4786fd5.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as i,a}from"./app.27759f01.js";const n={},o={"aria-hidden":"true","aria-label":"Minimize",class:"d-svg d-svg--system d-svg__minimize",viewBox:"0 0 24 24"},t=a("path",{d:"M6 19h12v2H6v-2z"},null,-1),c=[t];function _(r,d){return s(),i("svg",o,c)}var m=e(n,[["render",_],["__file","IconMinimize.vue"]]);export{m as default};
