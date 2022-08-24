c28e53b6852d21403f45daccbd262c74d6ae72a4
francisrupert <francisrupert@users.noreply.github.com>
2022-08-24 11:30:47 +0000

    Deploying to gh-pages from @ dialpad/dialtone@d2e39f2615daca05e4b29c3ae1b0f7ef9a3b533f 🚀

---
+++ deploy-previews/pr-643/assets/IconArrowRight.c7712023.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as r,a as s}from"./app.0417945e.js";const t={},a={"aria-hidden":"true","aria-label":"Arrow Right",class:"d-svg d-svg--system d-svg__arrowRight",viewBox:"0 0 24 24"},c=s("path",{d:"M9 7l5 5-5 5V7z"},null,-1),_=[c];function n(i,d){return o(),r("svg",a,_)}var h=e(t,[["render",n],["__file","IconArrowRight.vue"]]);export{h as default};
