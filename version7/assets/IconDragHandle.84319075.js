67758aedafe4f2995863b9a5548e93f86d104c39
francisrupert <francisrupert@gmail.com>
2022-08-30 16:00:18 +0000

    Deploying to gh-pages from @ dialpad/dialtone@71a302a37d0af207665a7c89e4a3845e7ce99cc9 🚀

--- version7/assets/IconDragHandle.84319075.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c as s,a as n}from"./app.23c0b4c6.js";const o={},r={"aria-hidden":"true","aria-label":"Drag Handle",class:"d-svg d-svg--system d-svg__dragHandle",viewBox:"0 0 24 24"},t=n("path",{d:"M20 9H4v2h16V9zM4 15h16v-2H4v2z"},null,-1),c=[t];function _(d,l){return a(),s("svg",r,c)}var v=e(o,[["render",_],["__file","IconDragHandle.vue"]]);export{v as default};
