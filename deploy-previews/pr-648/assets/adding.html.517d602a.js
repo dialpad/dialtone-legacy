c8b5c419a78eb9f747be737ac972b8823b37a756
juliodialpad <juliodialpad@users.noreply.github.com>
2022-08-30 15:52:30 +0000

    Deploying to gh-pages from @ dialpad/dialtone@ebe4dab2ec560646f6822e968813a1dee888046c 🚀

---
+++ deploy-previews/pr-648/assets/adding.html.517d602a.js
@@ -0,0 +1,1 @@
+const t=JSON.parse('{"key":"v-3bded04b","path":"/design/icons/adding.html","title":"Adding Icons","lang":"en-US","frontmatter":{"title":"Adding Icons","next":{"text":"Patterns","link":"/design/illustrations/patterns"}},"excerpt":"","headers":[],"git":{"updatedTime":1661874658000},"filePathRelative":"design/icons/adding.md"}');export{t as data};
