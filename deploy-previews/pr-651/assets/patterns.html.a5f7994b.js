911e5ad7dd81836f4d49e986a1bd7b33c6861a46
shefaligupta <shefaligupta@users.noreply.github.com>
2022-08-31 22:50:50 +0000

    Deploying to gh-pages from @ dialpad/dialtone@659ffd2d3409e2f912007c51e01c07d8ce2a1784 🚀

--- deploy-previews/pr-651/assets/patterns.html.a5f7994b.js
+++
@@ -1,1 +0,0 @@
-import{_ as n,r as a,o as r,c as s,b as e,d as o}from"./app.bddf91e9.js";const c={},i=o();function _(l,d){const t=a("icons");return r(),s("div",null,[e(t,{kind:"patterns",variation:"dark",title:"Dark Patterns"}),i,e(t,{kind:"patterns",variation:"light",class:"d-mt64",title:"Light Patterns"})])}var m=n(c,[["render",_],["__file","patterns.html.vue"]]);export{m as default};
