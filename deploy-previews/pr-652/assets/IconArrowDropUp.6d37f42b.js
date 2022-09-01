fb0cd3c2d8cd9138866400492b8e3cbbfec145ab
juliodialpad <juliodialpad@users.noreply.github.com>
2022-09-01 17:57:30 +0000

    Deploying to gh-pages from @ dialpad/dialtone@06c185200d79ae2b31fd347945cf47229c4f60a2 🚀

---
+++ deploy-previews/pr-652/assets/IconArrowDropUp.6d37f42b.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as r,a as s}from"./app.ae574e9d.js";const a={},t={"aria-hidden":"true","aria-label":"Arrow Drop Up",class:"d-svg d-svg--system d-svg__arrowDropUp",viewBox:"0 0 24 24"},c=s("path",{d:"M7 14l5-5 5 5H7z"},null,-1),_=[c];function n(d,p){return o(),r("svg",t,_)}var l=e(a,[["render",n],["__file","IconArrowDropUp.vue"]]);export{l as default};
