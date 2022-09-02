a9917e7e6c2b1f4c08f3c079de5f98f8ff920931
josedialpad <jose.silva@dialpad.com>
2022-09-02 17:27:20 +0000

    Deploying to gh-pages from @ dialpad/dialtone@5576578da8a69365fae167c2ece68ffc5b606ac9 🚀

--- assets/404.html.3ada443c.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,c}from"./app.dff3b821.js";const r={};function t(o,a){return _(),c("div")}var s=e(r,[["render",t],["__file","404.html.vue"]]);export{s as default};
---
+++ assets/color-palette.html.ad7d5991.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as c,c as r,b as l}from"./app.fcbf966a.js";const n={};function a(s,_){const e=t("colors");return c(),r("div",null,[l(e)])}var f=o(n,[["render",a],["__file","color-palette.html.vue"]]);export{f as default};
