2e135dff692062353e93ad220876128c8e7896e1
braddialpad <brad.paugh@dialpad.com>
2022-09-02 19:15:02 +0000

    Deploying to gh-pages from @ dialpad/dialtone@f5170976a0b3e7db89559d44b2c47b74eb1a2425 🚀

---
+++ assets/IconReply.21a9eaf8.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as a,a as c}from"./app.33eb06c6.js";const o={},t={"aria-hidden":"true","aria-label":"Reply",class:"d-svg d-svg--system d-svg__reply",viewBox:"0 0 24 24"},_=c("path",{d:"M10 9V5l-7 7 7 7v-4.1c5 0 8.5 1.6 11 5.1-1-5-4-10-11-11z"},null,-1),r=[_];function n(l,d){return s(),a("svg",t,r)}var p=e(o,[["render",n],["__file","IconReply.vue"]]);export{p as default};
