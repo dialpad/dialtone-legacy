acc74f8d9bbbaa623e66b828b661080408d7c711
braddialpad <braddialpad@users.noreply.github.com>
2022-09-06 17:34:20 +0000

    Deploying to gh-pages from @ dialpad/dialtone@b4619a01f69663d42a7b607dcb2bacb39e6ccb72 🚀

--- deploy-previews/pr-659/assets/IconStop.4b3a1007.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as o,a as t}from"./app.67df445d.js";const a={},c={"aria-hidden":"true","aria-label":"Stop",class:"d-svg d-svg--system d-svg__stop",viewBox:"0 0 24 24"},_=t("path",{d:"M5 5h14v14H5z"},null,-1),n=[_];function r(d,i){return s(),o("svg",c,n)}var p=e(a,[["render",r],["__file","IconStop.vue"]]);export{p as default};
