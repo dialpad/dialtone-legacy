1e8bc1bd6c144b6a68d0d3da24f4f09de4261c99
juliodialpad <juliodialpad@users.noreply.github.com>
2022-09-13 18:13:40 +0000

    Deploying to gh-pages from @ dialpad/dialtone@2147372205d8b49c5ec7d508cbff0f6a30bf90e1 🚀

---
+++ deploy-previews/pr-666/assets/patterns.html.667839dd.js
@@ -0,0 +1,1 @@
+import{_ as n,o as a,c as r,b as e,d as s,r as o}from"./app.d2e050ec.js";const c={},i=s();function _(l,d){const t=o("icons");return a(),r("div",null,[e(t,{kind:"patterns",variation:"dark",title:"Dark Patterns"}),i,e(t,{kind:"patterns",variation:"light",class:"d-mt64",title:"Light Patterns"})])}var m=n(c,[["render",_],["__file","patterns.html.vue"]]);export{m as default};
