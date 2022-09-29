83dea14fcb24dd8016c69e202a3f938e68b516b3
francisrupert <francisrupert@users.noreply.github.com>
2022-09-29 22:00:49 +0000

    Deploying to gh-pages from @ dialpad/dialtone@ef64f10d5d825a32f9fde99b1840393c6ee577c4 🚀

---
+++ deploy-previews/pr-702/assets/IconRemove.114bc2c7.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,a}from"./app.99075778.js";const c={},t={"aria-hidden":"true",focusable:"false","aria-label":"Remove",class:"d-svg d-svg--system d-svg__remove",viewBox:"0 0 24 24"},_=a("path",{d:"M19 13H5v-2h14v2Z"},null,-1),r=[_];function n(d,v){return s(),o("svg",t,r)}var i=e(c,[["render",n],["__file","IconRemove.vue"]]);export{i as default};
