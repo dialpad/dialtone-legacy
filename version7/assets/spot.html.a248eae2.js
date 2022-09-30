2ce23cd577f4b00e20e29dd40d13b45200d0e46c
braddialpad <brad.paugh@dialpad.com>
2022-09-30 20:55:13 +0000

    Deploying to gh-pages from @ dialpad/dialtone@224cd23465707a0bbdc6aa039b6f3e08827fbe1a 🚀

---
+++ version7/assets/spot.html.a248eae2.js
@@ -0,0 +1,1 @@
+import{_ as o,o as t,c as n,b as s,r as c}from"./app.21a92548.js";const r={};function _(a,l){const e=c("icons");return t(),n("div",null,[s(e,{kind:"spot",size:"large"})])}var p=o(r,[["render",_],["__file","spot.html.vue"]]);export{p as default};
