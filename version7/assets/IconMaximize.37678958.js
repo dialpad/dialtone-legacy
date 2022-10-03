38dd56aa2071b88d633cdc55d4f022697ec72708
francisrupert <francisrupert@gmail.com>
2022-10-03 19:05:17 +0000

    Deploying to gh-pages from @ dialpad/dialtone@c43d9b6a98cc42f0cf305e3551fbab0487fc19f1 🚀

--- version7/assets/IconMaximize.37678958.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as a,c as s,a as o}from"./app.eaed46ad.js";const c={},t={"aria-hidden":"true",focusable:"false","aria-label":"Maximize",class:"d-svg d-svg--system d-svg__maximize",viewBox:"0 0 24 24"},i=o("path",{d:"M3 3h18v2H3V3Z"},null,-1),_=[i];function n(r,d){return a(),s("svg",t,_)}var m=e(c,[["render",n],["__file","IconMaximize.vue"]]);export{m as default};
