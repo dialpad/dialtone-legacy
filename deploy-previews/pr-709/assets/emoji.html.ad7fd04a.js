9620315db5d7bf0aa84b325a64c7a37fc44a0604
francisrupert <francisrupert@users.noreply.github.com>
2022-10-18 15:51:05 +0000

    Deploying to gh-pages from @ dialpad/dialtone@94cad8b12ae32f0cf79a9104b995e0d8709fa0bf 🚀

---
+++ deploy-previews/pr-709/assets/emoji.html.ad7fd04a.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-de8fc100","path":"/design/icons/emoji.html","title":"Emoji","lang":"en-US","frontmatter":{"title":"Emoji","next":{"text":"Patterns","link":"/design/illustrations/patterns"}},"excerpt":"","headers":[],"git":{"updatedTime":1666108167000},"filePathRelative":"design/icons/emoji.md"}');export{e as data};
