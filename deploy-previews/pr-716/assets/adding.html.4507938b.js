718fceed2578be943c87cda281f21b71385c5711
modialpad <modialpad@users.noreply.github.com>
2022-10-21 20:52:47 +0000

    Deploying to gh-pages from @ dialpad/dialtone@c1378cb14ba1c4fbb2a6d5db6b8136281be36595 🚀

---
+++ deploy-previews/pr-716/assets/adding.html.4507938b.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-3bded04b","path":"/design/icons/adding.html","title":"Adding Icons","lang":"en-US","frontmatter":{"title":"Adding Icons","next":{"text":"Emoji","link":"/design/icons/emoji.html"}},"excerpt":"","headers":[],"git":{"updatedTime":1666385491000},"filePathRelative":"design/icons/adding.md"}');export{e as data};
