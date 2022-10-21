4b6610df8e6e22480f0e2efcc84a7a6606180af4
modialpad <modialpad@users.noreply.github.com>
2022-10-21 21:29:16 +0000

    Deploying to gh-pages from @ dialpad/dialtone@9da195019ba94679aa2eebc7f838c2f3bce9821f 🚀

--- deploy-previews/pr-716/assets/IconStop.78efa3cd.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t,a as o}from"./app.c4472e7e.js";const a={},c={"aria-hidden":"true",focusable:"false","aria-label":"Stop",class:"d-svg d-svg--system d-svg__stop",viewBox:"0 0 24 24"},_=o("rect",{x:"5",y:"5",width:"14",height:"14"},null,-1),r=[_];function n(d,i){return s(),t("svg",c,r)}var f=e(a,[["render",n],["__file","IconStop.vue"]]);export{f as default};
