89569db206a33a5c9f3d7b7b347735672d6e4576
juliodialpad <juliodialpad@users.noreply.github.com>
2022-10-27 22:50:32 +0000

    Deploying to gh-pages from @ dialpad/dialtone@488004048e9c3ed58b817f2dfca7dea6a5d8bfbe 🚀

--- deploy-previews/pr-719/assets/emoji.html.c56cae8b.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-de8fc100","path":"/design/icons/emoji.html","title":"Emoji","lang":"en-US","frontmatter":{"title":"Emoji","next":{"text":"Patterns","link":"/design/illustrations/patterns"}},"excerpt":"","headers":[],"git":{"updatedTime":1666893564000},"filePathRelative":"design/icons/emoji.md"}');export{e as data};
--- deploy-previews/pr-719/assets/spot.html.ed72f7ae.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as t,c as n,b as s,r as c}from"./app.13d6a8b2.js";const r={};function _(a,l){const e=c("icons");return t(),n("div",null,[s(e,{kind:"spot",size:"large"})])}var p=o(r,[["render",_],["__file","spot.html.vue"]]);export{p as default};
