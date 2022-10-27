01d97fb066f450cfda98d1124c6eb09d1b370875
juliodialpad <juliodialpad@users.noreply.github.com>
2022-10-27 18:00:46 +0000

    Deploying to gh-pages from @ dialpad/dialtone@83156c524abe5c25e033fcf60ddd2bff371f6698 🚀

---
+++ deploy-previews/pr-719/assets/localization.html.731e7c6f.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./app.13d6a8b2.js";const t={};function a(r,_){return c(),o("div")}var n=e(t,[["render",a],["__file","localization.html.vue"]]);export{n as default};
---
+++ deploy-previews/pr-719/assets/theme.html.75b05285.js
@@ -0,0 +1,149 @@
+import{_ as d,o as e,c as n,e as s}from"./app.13d6a8b2.js";const t={},a=s(`<table class="d-table dialtone-doc-table d-mt16"><thead><tr><th scope="col">
+        Section
+      </th> <th scope="col">
+        Item
+      </th> <th scope="col">
+        Property
+      </th> <th scope="col">
+        Variable
+      </th> <th scope="col">
+        CSS Utility
+      </th></tr></thead> <tbody><tr><th scope="row" class="d-fw-normal">
+        Topbar
+      </th> <td class="d-fw-normal">
+        Background
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-topbar-color-background)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-topbar-fc&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-topbar-fc"></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Topbar
+      </th> <td class="d-fw-normal">
+        Text
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-topbar-color-hsl)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-topbar-fc&quot;
+          </div> <div class="d-fl0 d-fs-200 d-m4 d-ta-center d-w32 d-lh4 d-ml16 d-theme-topbar-fc">
+            Aa
+          </div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Background
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-sidebar-color-background)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-sidebar-bgc&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-sidebar-bgc"></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Text: Name / Group
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-sidebar-color-hsl)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-sidebar-bgc&quot;
+          </div> <div class="d-fl0 d-fs-200 d-m4 d-ta-center d-w32 d-lh4 d-ml16 d-theme-sidebar-fc">
+            Aa
+          </div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Text: Status
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-sidebar-color-hsl)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-sidebar-fc d-fco75&quot;
+          </div> <div class="d-fl0 d-fs-200 d-lh4 d-m4 d-ta-center d-w32 d-theme-sidebar-fc d-ml16 d-fco75">
+            Aa
+          </div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Icon
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-sidebar-color-hsl)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-sidebar-fc d-fco85 h:d-fco100&quot;
+          </div> <div class="d-fl0 d-fs-200 d-lh4 d-theme-sidebar-fc d-ta-center d-w32 d-m4 d-ml16 d-fco85 h:d-fco100"><svg aria-hidden="true" aria-label="Info" class="d-svg d-svg--system d-svg__info" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-6h2v6zm0-8h-2V7h2v2z"></path></svg></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Background: Active Row
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-sidebar-active-row-color-background)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-sidebar-row-active-bgc&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-sidebar-row-active-bgc"></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Text: Active Row
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-sidebar-active-row-color-hsl)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-sidebar-row-active-fc&quot;
+          </div> <div class="d-fl0 d-fs-200 d-m4 d-ta-center d-w32 d-lh4 d-ml16 d-theme-sidebar-row-active-fc">
+            Aa
+          </div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Background: Hover Row Color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-sidebar-row-color-background-hover)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-sidebar-row-bgc&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-sidebar-row-bgc"></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Sidebar
+      </th> <td class="d-fw-normal">
+        Background: Mention Badges
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-mention-color-background)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-mention&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-mention"></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Presence
+      </th> <td class="d-fw-normal">
+        Available
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-presence-color-background-available)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-presence-available&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-presence-available"></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Presence
+      </th> <td class="d-fw-normal">
+        Actively Busy
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-presence-color-background-busy-unavailable)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-presence-busy-unavailable&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-presence-busy-unavailable"></div></div></td></tr> <tr><th scope="row" class="d-fw-normal">
+        Presence
+      </th> <td class="d-fw-normal">
+        Busy
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        background-color
+      </td> <td class="d-ff-mono d-fc-purple-400 d-fw-normal d-fs-100">
+        var(--theme-presence-color-background-busy)
+      </td> <td><div class="d-d-flex d-jc-space-between d-ai-center"><div class="d-fl1 d-ff-mono d-fs-100">
+            &quot;d-theme-presence-busy&quot;
+          </div> <div class="d-fl-shrink0 d-m4 d-ml16 d-h32 d-w32 d-bar4 d-ba d-bc-black-100 d-theme-presence-busy"></div></div></td></tr></tbody></table>`,1),o=[a];function c(l,r){return e(),n("div",null,o)}var m=d(t,[["render",c],["__file","theme.html.vue"]]);export{m as default};
