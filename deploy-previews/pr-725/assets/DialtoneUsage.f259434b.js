04e3f25e9e8fa9783d302c863e49e036c4010c71
josedialpad <josedialpad@users.noreply.github.com>
2022-11-01 16:14:57 +0000

    Deploying to gh-pages from @ dialpad/dialtone@5fde0cf014b3f5ce8a4cf65921bd136cad70d182 🚀

--- deploy-previews/pr-725/assets/DialtoneUsage.f259434b.js
+++
@@ -1,5 +0,0 @@
-import{_ as c,o as i,c as l,a as e,b as t,g as _,d as o,r as a}from"./app.3d19d698.js";const r={name:"DialtoneUsage"},h={class:"dialtone-usage"},g={class:"dialtone-usage__item dialtone-usage__item--do"},u={class:"dialtone-usage__hd dialtone-usage__hd--do"},m=o(`
-        Do
-      `),p=o(),v={class:"dialtone-usage__bd"},f=o(),k={class:"dialtone-usage__item dialtone-usage__item--dont"},D={class:"dialtone-usage__hd dialtone-usage__hd--dont"},$=o(`
-        Don\u2019t
-      `),b=o(),B={class:"dialtone-usage__bd"};function N(s,U,V,x,C,E){const n=a("icon-checkmark"),d=a("icon-close");return i(),l("div",h,[e("div",g,[e("h3",u,[t(n),m]),p,e("div",v,[_(s.$slots,"do")])]),f,e("div",k,[e("h3",D,[t(d),$]),b,e("div",B,[_(s.$slots,"dont")])])])}var T=c(r,[["render",N],["__file","DialtoneUsage.vue"]]);export{T as D};
--- deploy-previews/pr-725/assets/contractions.html.8adebc75.js
+++
@@ -1,1 +0,0 @@
-const t=JSON.parse('{"key":"v-7d295147","path":"/guides/content/contractions.html","title":"Contractions","lang":"en-US","frontmatter":{"title":"Contractions","desc":"We use contractions often. They help us sound human and friendly."},"excerpt":"","headers":[],"git":{"updatedTime":1667247245000},"filePathRelative":"guides/content/contractions.md"}');export{t as data};
