0945ce5250dce5c1e1b181cdf9597f28bfb733ab
Jose Silva <jose.silva@dialpad.com>
2022-11-01 12:36:45 -0300

    feat(toggle): update example toggle component

--- docs/.vuepress/exampleComponents/ExampleToggle.vue
+++ docs/.vuepress/exampleComponents/ExampleToggle.vue
@@ -10,11 +10,11 @@
     <button
       id="Dialtone-Toggle1"
       class="d-toggle"
-      :class="{ 'd-toggle--checked': isChecked }"
-      :aria-checked="isChecked"
+      :class="toggleClasses"
+      :aria-checked="toggleAriaChecked"
       :disabled="disabled"
       type="button"
-      role="switch"
+      :role="toggleRole"
       @click="toggleState"
     >
       <span class="d-toggle__inner" />
@@ -40,16 +40,42 @@ export default {
       type: Boolean,
       default: false,
     },
+
+    indeterminate: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   data () {
     return {
       isChecked: this.checked,
+      isIndeterminate: this.indeterminate,
     };
+  },
+
+  computed: {
+    toggleClasses () {
+      return [
+        {
+          'd-toggle--checked': this.isChecked,
+          'd-toggle--indeterminate': this.isIndeterminate,
+        },
+      ];
+    },
+
+    toggleRole () {
+      return this.isIndeterminate ? 'checkbox' : 'switch';
+    },
+
+    toggleAriaChecked () {
+      return this.isIndeterminate ? 'mixed' : this.isChecked;
+    },
   },
 
   methods: {
     toggleState () {
+      this.isIndeterminate = null;
       this.isChecked = !this.isChecked;
     },
   },
