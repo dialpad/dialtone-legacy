2476769a3260788be9b1066462a3a3a8b2e5d5b0
braddialpad <braddialpad@users.noreply.github.com>
2022-11-08 19:42:21 +0000

    Deploying to gh-pages from @ dialpad/dialtone@bf495dfbd66ad60a1582853b746b5eac75660d9b 🚀

---
+++ deploy-previews/pr-650/assets/patterns.html.496da869.js
@@ -0,0 +1,1 @@
+import{_ as n,r as a,o as r,c as s,b as e,d as o}from"./app.aa3c0246.js";const c={},i=o();function _(l,d){const t=a("icons");return r(),s("div",null,[e(t,{kind:"patterns",variation:"dark",title:"Dark Patterns"}),i,e(t,{kind:"patterns",variation:"light",class:"d-mt64",title:"Light Patterns"})])}var m=n(c,[["render",_],["__file","patterns.html.vue"]]);export{m as default};
