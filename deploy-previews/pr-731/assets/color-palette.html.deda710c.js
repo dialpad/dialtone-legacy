cfdbb66c34463c927bde11ca6c85cec4a81cd7cd
francisrupert <francisrupert@users.noreply.github.com>
2022-11-09 14:57:05 +0000

    Deploying to gh-pages from @ dialpad/dialtone@11cac52e8378600ce71589471c3e3503830464a7 🚀

--- deploy-previews/pr-731/assets/color-palette.html.deda710c.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as c,c as r,d as l}from"./app.0c8bf3f7.js";const n={};function a(s,_){const e=t("colors");return c(),r("div",null,[l(e)])}var f=o(n,[["render",a],["__file","color-palette.html.vue"]]);export{f as default};
---
+++ deploy-previews/pr-731/assets/localization.html.7d0563c1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./app.1b0e0b34.js";const t={};function a(r,_){return c(),o("div")}var n=e(t,[["render",a],["__file","localization.html.vue"]]);export{n as default};
