e22ea5b9c35c4d220631c0a6b994a3b527f5f6ff
juliodialpad <juliodialpad@users.noreply.github.com>
2022-11-10 21:33:51 +0000

    Deploying to gh-pages from @ dialpad/dialtone@e6ef6b61fb0553fbac8c6ea1664439974a541a8e 🚀

--- deploy-previews/pr-734/assets/spot.html.d1c04c1b.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as n,c as s,d as c}from"./app.56727463.js";const r={};function _(a,l){const e=t("icons");return n(),s("div",null,[c(e,{kind:"spot",size:"large"})])}var p=o(r,[["render",_],["__file","spot.html.vue"]]);export{p as default};
