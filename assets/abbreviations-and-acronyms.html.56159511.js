60ea61155aca86fe5be85dc382c7fc20336f987d
juliodialpad <87546543+juliodialpad@users.noreply.github.com>
2022-11-12 00:15:04 +0000

    Deploying to gh-pages from @ dialpad/dialtone@17e1ddf0c62abf8ddf3b208ba8a817c2d167527c 🚀

---
+++ assets/abbreviations-and-acronyms.html.56159511.js
@@ -0,0 +1,5 @@
+import{_ as a,o as l,c as n,a as t,d as s,w as o,b as e}from"./app.6144beb2.js";import{D as c}from"./DialtoneUsage.1c141f60.js";const r=e("p",null,"Because contact center users are more specialized than talk users, it may occasionally be clearer to use a common acronym or abbreviation.",-1),i=e("ul",null,[e("li",null,"Unmute microphone")],-1),d=e("ul",null,[e("li",null,"Unmute mic")],-1),u=e("table",{class:"d-table dialtone-doc-table d-body-base"},[e("thead",null,[e("tr",null,[e("th",{class:"d-w25p",scope:"col"},`
+        Abbreviation
+      `),t(),e("th",{scope:"col"},`
+        Description
+      `)])]),t(),e("tbody",null,[e("tr",null,[e("th",{scope:"row"},"KB"),t(),e("td",null,"Kilobytes")])])],-1),_={__name:"abbreviations-and-acronyms.html",setup(m){return(b,p)=>(l(),n("div",null,[r,t(),s(c,null,{do:o(()=>[i]),dont:o(()=>[d]),_:1}),t(),u]))}};var v=a(_,[["__file","abbreviations-and-acronyms.html.vue"]]);export{v as default};
