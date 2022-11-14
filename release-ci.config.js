86207ad04609720d70861a88f306700bc002fb30
Jose Silva <jose.silva@dialpad.com>
2022-11-14 12:28:41 -0300

    chore(semantic-release): add refactor type to release patch version

--- release-ci.config.js
+++ release-ci.config.js
@@ -12,7 +12,12 @@ module.exports = {
     },
   ],
   plugins: [
-    '@semantic-release/commit-analyzer',
+    ['@semantic-release/commit-analyzer', {
+      preset: 'angular',
+      releaseRules: [
+        { type: 'refactor', release: 'patch' },
+      ],
+    }],
     ['@semantic-release/release-notes-generator', {
       config: '@dialpad/conventional-changelog-angular',
     }],
