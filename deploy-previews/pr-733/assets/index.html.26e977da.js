1a6ee905add882b3a1e68e7beeac608e5f5667d3
francisrupert <francisrupert@users.noreply.github.com>
2022-11-14 17:10:45 +0000

    Deploying to gh-pages from @ dialpad/dialtone@497b96f4b46660b5e75c88dadc223183c82e2051 🚀

--- deploy-previews/pr-733/assets/index.html.26e977da.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-ff762602","path":"/guides/","title":"Guides","lang":"en-US","frontmatter":{"title":"Guides","desc":null,"no_preview":true,"prev":{"text":"Spacing system","link":"/design/spacing/spacing-system.html"}},"excerpt":"","headers":[],"git":{"updatedTime":1668204409000},"filePathRelative":"guides/index.md"}');export{e as data};
