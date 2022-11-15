15bfc93afe48d577f86faf4c7bb6b55066b804e0
francisrupert <francisrupert@users.noreply.github.com>
2022-11-15 03:29:54 +0000

    Deploying to gh-pages from @ dialpad/dialtone@9dd380c0b0f9bb17b46a7079ef28491a71aa107e 🚀

---
+++ deploy-previews/pr-733/assets/index.html.2b40f40e.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-171828c8","path":"/design/typography/","title":"Typography","lang":"en-US","frontmatter":{"title":"Typography","desc":"Lorem ipsum dolor sit amet."},"excerpt":"","headers":[],"git":{"updatedTime":1668482890000},"filePathRelative":"design/typography/index.md"}');export{e as data};
