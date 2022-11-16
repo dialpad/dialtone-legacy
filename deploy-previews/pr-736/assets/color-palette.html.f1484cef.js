cd21e1d26f7c73aba967912fd60a863a0d8b5069
modialpad <modialpad@users.noreply.github.com>
2022-11-16 20:34:32 +0000

    Deploying to gh-pages from @ dialpad/dialtone@339c5a1b4c85b27061cbd0adc0c77024749b0e14 🚀

---
+++ deploy-previews/pr-736/assets/color-palette.html.f1484cef.js
@@ -0,0 +1,1 @@
+import{_ as o,r as t,o as c,c as r,d as l}from"./app.bbc31e25.js";const n={};function a(s,_){const e=t("colors");return c(),r("div",null,[l(e)])}var f=o(n,[["render",a],["__file","color-palette.html.vue"]]);export{f as default};
