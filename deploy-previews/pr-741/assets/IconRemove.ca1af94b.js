b40add5e4284906db4eea64dd22900860ac13b87
josedialpad <josedialpad@users.noreply.github.com>
2022-11-18 15:07:22 +0000

    Deploying to gh-pages from @ dialpad/dialtone@d0cc8f8095701cc9581a295de34786e7b64b9bfd 🚀

---
+++ deploy-previews/pr-741/assets/IconRemove.ca1af94b.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as o,b as a}from"./app.35ec70fd.js";const c={},t={"aria-hidden":"true",focusable:"false","aria-label":"Remove",class:"d-svg d-svg--system d-svg__remove",viewBox:"0 0 24 24"},_=a("path",{d:"M19 13H5v-2h14v2Z"},null,-1),r=[_];function n(d,v){return s(),o("svg",t,r)}var i=e(c,[["render",n],["__file","IconRemove.vue"]]);export{i as default};
---
+++ deploy-previews/pr-741/assets/index.html.45897497.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-72582dd0","path":"/design/","title":"Design language","lang":"en-US","frontmatter":{"title":"Design language","desc":"Foundational visual attributes establishing the distinct look and feel of Dialpad products."},"excerpt":"","headers":[],"git":{"updatedTime":1668783948000},"filePathRelative":"design/index.md"}');export{e as data};
