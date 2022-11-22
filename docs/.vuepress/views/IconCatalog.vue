bb1d7107135c44c108d5b7c5ab3c8796c1481db5
Jose Silva <jose.silva@dialpad.com>
2022-11-22 18:52:50 -0300

    docsite: add search by icon name in icon component

--- docs/.vuepress/views/IconCatalog.vue
+++ docs/.vuepress/views/IconCatalog.vue
@@ -1,6 +1,27 @@
 <template>
-  <div class="d-mt8 d-mb16">
-    <div class="d-select">
+  <div class="d-d-grid d-gg16 d-g-cols6 d-mt8 d-mb16">
+    <div class="d-gc4">
+      <div class="d-input__wrapper">
+        <span class="d-input-icon d-input-icon--left">
+          <icon-search />
+        </span>
+        <input
+          id="search-input"
+          ref="searchRef"
+          v-model="search"
+          aria-label="Search icon"
+          class="d-input d-input-icon--left d-input-icon--right"
+          type="text"
+        >
+        <span class="d-input-icon d-input-icon--right">
+          <icon-x-circle
+            class="d-c-pointer"
+            @click="resetSearch"
+          />
+        </span>
+      </div>
+    </div>
+    <div class="d-gc2 d-select">
       <label
         class="d-vi-visible-sr"
         for="Dialtone--SelectCategory"
@@ -55,14 +76,49 @@
 import BaseIcon from '@baseComponents/BaseV7Icon.vue';
 import { categories } from '@data/icon.json';
 import { computed, ref } from 'vue';
+import IconSearch from '@v7Icons/Search.vue';
+import IconXCircle from '@v7Icons/XCircle.vue';
+import { refDebounced } from '@vueuse/core';
 
 const selectedIcon = ref(null);
 const selectedCategory = ref('all');
+const search = ref(null);
+const searchDebounced = refDebounced(search, 500);
+const searchRef = ref(null);
 
 const categoriesList = computed(() => {
   return Object.keys(categories);
 });
+
+const searchByIconName = (icons, name) => {
+  const result = {};
+  const regex = new RegExp(name, 'i');
+
+  for (const category in icons) {
+    for (const icon in icons[category]) {
+      if (regex.test(icon)) {
+        if (result[category] === undefined) {
+          result[category] = {};
+        }
+
+        result[category][icon] = icons[category][icon];
+      }
+    }
+  }
+
+  return result;
+};
+
+const resetSearch = () => { search.value = null; searchDebounced.value = null; searchRef.value.focus(); };
+
+const resetCategory = () => { selectedCategory.value = 'all'; };
+
 const iconsList = computed(() => {
+  if (searchDebounced.value != null && searchDebounced.value !== '') {
+    resetCategory();
+    return searchByIconName(categories, searchDebounced.value);
+  }
+
   return selectedCategory.value === 'all'
     ? categories
     : Object.assign({}, { [selectedCategory.value]: categories[selectedCategory.value] });
