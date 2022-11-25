f1f4fbb49eadaec76abc97cc7bc1779a090d99e9
braddialpad <brad.paugh@dialpad.com>
2022-11-25 00:14:01 +0000

    Deploying to gh-pages from @ dialpad/dialtone@bac390f9724f5c66db9d61fe8e712fcb313dbb51 🚀

---
+++ assets/index.html.f6ceee61.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-171828c8","path":"/design/typography/","title":"Typography","lang":"en-US","frontmatter":{"title":"Typography","desc":"Guidance for clear, legible, and easy-to-read text."},"excerpt":"","headers":[],"git":{"updatedTime":1669335045000},"filePathRelative":"design/typography/index.md"}');export{e as data};
