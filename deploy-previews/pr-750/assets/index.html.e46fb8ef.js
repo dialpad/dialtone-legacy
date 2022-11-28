42843774857379b8f69744eebd054ffdedd05b07
tedgoas-dialpad <tedgoas-dialpad@users.noreply.github.com>
2022-11-28 22:28:30 +0000

    Deploying to gh-pages from @ dialpad/dialtone@b308cedde35becf9ccda137e623550d034ea839f 🚀

--- deploy-previews/pr-750/assets/index.html.e46fb8ef.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-72582dd0","path":"/design/","title":"Design language","lang":"en-US","frontmatter":{"title":"Design language","desc":"The visual foundation that supports and unites Dialpad products."},"excerpt":"","headers":[],"git":{"updatedTime":1669667386000},"filePathRelative":"design/index.md"}');export{e as data};
--- deploy-previews/pr-750/assets/skeleton.html.6c32daaa.js
+++
@@ -1,1 +0,0 @@
-import{_ as l,r as o,o as t,c as a,d as n,w as s,b as c}from"./app.8f71e708.js";const r={},_=c("div",{class:"d-h8 d-bar2 skeleton-placeholder skeleton-placeholder--animate"},null,-1);function d(h,i){const e=o("code-well-header");return t(),a("div",null,[n(e,null,{default:s(()=>[_]),_:1})])}var f=l(r,[["render",d],["__file","skeleton.html.vue"]]);export{f as default};
