d64fbd4ed28546900cc73a54ed2503c0b9287bc7
braddialpad <braddialpad@users.noreply.github.com>
2022-11-29 21:26:53 +0000

    Deploying to gh-pages from @ dialpad/dialtone@24506d125c80df73ce5902afcb9ea59d231b8a8e 🚀

---
+++ deploy-previews/pr-753/assets/IconAdd.97d588b9.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as a,b as d}from"./app.c730ef06.js";const o={},c={"aria-hidden":"true",focusable:"false","aria-label":"Add",class:"d-svg d-svg--system d-svg__add",viewBox:"0 0 24 24"},t=d("path",{d:"M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2Z"},null,-1),_=[t];function n(r,l){return s(),a("svg",c,_)}var h=e(o,[["render",n],["__file","IconAdd.vue"]]);export{h as default};
