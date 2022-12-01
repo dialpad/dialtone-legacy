a6efe923f05cf49c1036137da1e516b270ea29d2
Julio Ortega <julio.ortega@dialpad.com>
2022-11-30 18:05:32 -0600

    docs: fix styling

--- docs/.vuepress/theme/components/PageToc.vue
+++ docs/.vuepress/theme/components/PageToc.vue
@@ -1,7 +1,7 @@
 <template>
   <aside
     v-if="$page.headers.length > 0"
-    class="toc d-pt24"
+    class="toc d-t64 d-ps-sticky d-pt24 d-as-flex-start d-zi-navigation d-pr32 d-w216"
   >
     <h3 class="d-pl8 d-fs-100 d-pb8 d-ws-nowrap d-tt-uppercase d-fw-semibold d-fc-secondary">
       On this page
