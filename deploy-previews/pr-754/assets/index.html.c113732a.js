96a94703479404ce6f52736830f6536a7976d035
francisrupert <francisrupert@users.noreply.github.com>
2022-12-02 23:44:27 +0000

    Deploying to gh-pages from @ dialpad/dialtone@faab2397d3631c58b3be558f76bbb180883adbe0 🚀

---
+++ deploy-previews/pr-754/assets/index.html.c113732a.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-171828c8","path":"/design/typography/","title":"Typography","lang":"en-US","frontmatter":{"title":"Typography","desc":"Guidance for clear, legible, and easy-to-read text."},"excerpt":"","headers":[],"git":{"updatedTime":1670024560000},"filePathRelative":"design/typography/index.md"}');export{e as data};
