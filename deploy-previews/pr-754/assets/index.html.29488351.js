d2235583ebd8c7290a5907e907fc679abe00451d
francisrupert <francisrupert@users.noreply.github.com>
2022-12-05 16:19:10 +0000

    Deploying to gh-pages from @ dialpad/dialtone@cf552d464d76b713fd1c9f6f9597ad7fe695745f 🚀

---
+++ deploy-previews/pr-754/assets/index.html.29488351.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-72582dd0","path":"/design/","title":"Design language","lang":"en-US","frontmatter":{"title":"Design language","desc":"The visual foundation that supports and unites Dialpad products."},"excerpt":"","headers":[],"git":{"updatedTime":1670257070000},"filePathRelative":"design/index.md"}');export{e as data};
