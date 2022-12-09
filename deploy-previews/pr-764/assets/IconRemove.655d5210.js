0f1f7052911b7fbbae566afd8f2067fcdeaac75c
juliodialpad <juliodialpad@users.noreply.github.com>
2022-12-09 06:33:47 +0000

    Deploying to gh-pages from @ dialpad/dialtone@89e0b3230967f03ee04717683f7deb735f4cf651 🚀

--- deploy-previews/pr-764/assets/IconRemove.655d5210.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as o,b as c}from"./app.ab3aa649.js";const t={},a={"aria-hidden":"true",focusable:"false","aria-label":"Remove",class:"d-svg d-svg--system d-svg__remove",viewBox:"0 0 24 24"},_=c("path",{d:"M19 13H5v-2h14v2Z"},null,-1),n=[_];function r(d,l){return s(),o("svg",a,n)}const i=e(t,[["render",r],["__file","IconRemove.vue"]]);export{i as default};
