86ffaa51000c6f8b52b0af3c3081283a81e04648
francisrupert <francisrupert@users.noreply.github.com>
2022-12-13 19:21:23 +0000

    Deploying to gh-pages from @ dialpad/dialtone@fad175c1c863d55aa70b0f53f2aee92e35e01942 🚀

---
+++ deploy-previews/pr-763/assets/IconPlay.36167635.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as a,b as o}from"./app.ffb7bb7f.js";const c={},t={"aria-hidden":"true",focusable:"false","aria-label":"Play",class:"d-svg d-svg--system d-svg__play",viewBox:"0 0 24 24"},_=o("path",{d:"M8 5v14l11-7L8 5Z"},null,-1),l=[_];function n(r,d){return s(),a("svg",t,l)}const f=e(c,[["render",n],["__file","IconPlay.vue"]]);export{f as default};
