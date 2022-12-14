0caa383b7fbb920e435763471c7fc316c982c550
francisrupert <francisrupert@users.noreply.github.com>
2022-12-14 16:55:04 +0000

    Deploying to gh-pages from @ dialpad/dialtone@3f913090eb96e7446cfac8261ce8c7b89a6f4402 🚀

---
+++ deploy-previews/pr-763/assets/DialtoneUsage.08368f69.js
@@ -0,0 +1,5 @@
+import{_,r as d,o as i,c,b as e,e as a,a as s,g as n}from"./app.b4280157.js";const l={name:"DialtoneUsage"},r={class:"dialtone-usage"},u={class:"dialtone-usage__item dialtone-usage__item--do"},h={class:"dialtone-usage__hd dialtone-usage__hd--do"},g={class:"dialtone-usage__bd"},m={class:"dialtone-usage__item dialtone-usage__item--dont"},p={class:"dialtone-usage__hd dialtone-usage__hd--dont"},v={class:"dialtone-usage__bd"};function f(o,D,$,b,B,N){const t=d("dt-icon");return i(),c("div",r,[e("div",u,[e("h3",h,[a(t,{name:"thumbs-up"}),s(`
+        Do
+      `)]),s(),e("div",g,[n(o.$slots,"do")])]),s(),e("div",m,[e("h3",p,[a(t,{name:"thumbs-down"}),s(`
+        Don’t
+      `)]),s(),e("div",v,[n(o.$slots,"dont")])])])}const V=_(l,[["render",f],["__file","DialtoneUsage.vue"]]);export{V as D};
