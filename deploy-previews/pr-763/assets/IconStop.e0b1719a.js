a61465da10816337ed880918969e701249a8b412
francisrupert <francisrupert@users.noreply.github.com>
2022-12-15 22:09:46 +0000

    Deploying to gh-pages from @ dialpad/dialtone@8b09c136a1b7c9ffec6fc29b7c4739db66409b28 🚀

--- deploy-previews/pr-763/assets/IconStop.e0b1719a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c as t,b as o}from"./app.ae4c91c3.js";const c={},a={"aria-hidden":"true",focusable:"false","aria-label":"Stop",class:"d-svg d-svg--system d-svg__stop",viewBox:"0 0 24 24"},_=o("rect",{x:"5",y:"5",width:"14",height:"14"},null,-1),n=[_];function r(d,i){return s(),t("svg",a,n)}const f=e(c,[["render",r],["__file","IconStop.vue"]]);export{f as default};
