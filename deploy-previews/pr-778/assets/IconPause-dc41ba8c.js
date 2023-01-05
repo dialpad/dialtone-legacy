d23ca2ef94697214ef802593386090b114aa9e12
francisrupert <francisrupert@users.noreply.github.com>
2023-01-05 04:37:48 +0000

    Deploying to gh-pages from @ dialpad/dialtone@7a4493f19b23d664fbe3907d0e734c6d7ba876cf 🚀

---
+++ deploy-previews/pr-778/assets/IconPause-dc41ba8c.js
@@ -0,0 +1,1 @@
+import{_ as e,v as s,x as a,y as o}from"./framework-bc192251.js";const t={},c={"aria-hidden":"true",focusable:"false","aria-label":"Pause",class:"d-svg d-svg--system d-svg__pause",viewBox:"0 0 24 24"},_=o("path",{d:"M6 19h4V5H6v14Zm8-14v14h4V5h-4Z"},null,-1),n=[_];function r(d,l){return s(),a("svg",c,n)}const u=e(t,[["render",r],["__file","IconPause.vue"]]);export{u as default};
