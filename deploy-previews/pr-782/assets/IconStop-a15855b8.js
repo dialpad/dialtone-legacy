7e3bd698b677319e7d58c1f82f5c3a1701b5d5c9
josedialpad <josedialpad@users.noreply.github.com>
2023-01-07 01:05:09 +0000

    Deploying to gh-pages from @ dialpad/dialtone@2c823c82bd90cda7cc80c4f116bccb5e532d5628 🚀

---
+++ deploy-previews/pr-782/assets/IconStop-a15855b8.js
@@ -0,0 +1,1 @@
+import{_ as e,v as s,x as t,y as o}from"./framework-bc192251.js";const c={},a={"aria-hidden":"true",focusable:"false","aria-label":"Stop",class:"d-svg d-svg--system d-svg__stop",viewBox:"0 0 24 24"},_=o("rect",{x:"5",y:"5",width:"14",height:"14"},null,-1),n=[_];function r(d,i){return s(),t("svg",a,n)}const f=e(c,[["render",r],["__file","IconStop.vue"]]);export{f as default};
