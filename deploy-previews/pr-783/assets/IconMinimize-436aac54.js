f065b37feb63d409bccca1c6e45948eb2e47733c
francisrupert <francisrupert@users.noreply.github.com>
2023-01-09 19:49:09 +0000

    Deploying to gh-pages from @ dialpad/dialtone@d2979cecda09cd6c53d832c75a63db88ce768868 🚀

---
+++ deploy-previews/pr-783/assets/IconMinimize-436aac54.js
@@ -0,0 +1,1 @@
+import{_ as e,v as s,x as i,y as n}from"./framework-bc192251.js";const o={},t={"aria-hidden":"true",focusable:"false","aria-label":"Minimize",class:"d-svg d-svg--system d-svg__minimize",viewBox:"0 0 24 24"},a=n("path",{d:"M6 19h12v2H6v-2Z"},null,-1),c=[a];function _(r,d){return s(),i("svg",t,c)}const m=e(o,[["render",_],["__file","IconMinimize.vue"]]);export{m as default};
