fcec75151a9dc6dd5e99161041aa6cee2d9736d3
francisrupert <francisrupert@users.noreply.github.com>
2023-01-10 18:54:32 +0000

    Deploying to gh-pages from @ dialpad/dialtone@4413baf2209d7b2233a50fe956a32ff692810c0a 🚀

---
+++ deploy-previews/pr-786/assets/lazy-show.html-17c023cc.js
@@ -0,0 +1,3 @@
+import{_ as n,s as o,v as a,x as c,y as e,G as d,C as t}from"./framework-bc192251.js";const i={},l={class:"d-notice d-notice--info d-mt24 d-wmx100p",role:"status","aria-hidden":"false"},_={class:"d-notice__icon"},r=e("div",{class:"d-notice__content d-stack4"},[e("p",{class:"d-notice__message"},[t(`
+      Currently documented in `),e("a",{target:"_blank",href:"https://vue.dialpad.design/?path=/docs/utilities-lazy-show--default",class:"d-link d-link--muted"},"Dialtone Vue"),t(`.
+    `)])],-1);function m(h,u){const s=o("dt-icon");return a(),c("div",null,[e("aside",l,[e("div",_,[d(s,{name:"info"})]),t(),r])])}const p=n(i,[["render",m],["__file","lazy-show.html.vue"]]);export{p as default};
