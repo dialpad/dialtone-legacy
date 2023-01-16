017f3d9fc982b8e4d6480eceb60643283338a7ec
braddialpad <brad.paugh@dialpad.com>
2023-01-16 21:30:02 +0000

    Deploying to gh-pages from @ dialpad/dialtone@453496a8c34282e0e6b19b21d4d3d645dbd54a78 🚀

--- assets/IconRemove-cc429b50.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,v as s,x as o,y as t}from"./framework-bc192251.js";const a={},c={"aria-hidden":"true",focusable:"false","aria-label":"Remove",class:"d-svg d-svg--system d-svg__remove",viewBox:"0 0 24 24"},_=t("path",{d:"M19 13H5v-2h14v2Z"},null,-1),n=[_];function r(d,v){return s(),o("svg",c,n)}const i=e(a,[["render",r],["__file","IconRemove.vue"]]);export{i as default};
