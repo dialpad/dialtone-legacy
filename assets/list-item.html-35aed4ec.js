23a53226ee009195c8deca79fb07b38fd2cce230
josedialpad <jose.silva@dialpad.com>
2023-01-19 19:04:36 +0000

    Deploying to gh-pages from @ dialpad/dialtone@d700ce8e3ce1e9913d6f9d5f79a88a965367e4be 🚀

--- assets/list-item.html-35aed4ec.js
+++
@@ -1,3 +0,0 @@
-import{_ as n,s as o,v as c,x as a,y as e,G as d,C as t}from"./framework-c9271261.js";const i={},_={class:"d-notice d-notice--info d-mt24 d-wmx100p",role:"status","aria-hidden":"false"},l={class:"d-notice__icon"},r=e("div",{class:"d-notice__content d-stack4"},[e("p",{class:"d-notice__message"},[t(`
-      Currently documented in `),e("a",{target:"_blank",href:"https://vue.dialpad.design/?path=/docs/components-list-item--default",class:"d-link d-link--muted"},"Dialtone Vue"),t(`.
-    `)])],-1);function m(f,p){const s=o("dt-icon");return c(),a("div",null,[e("aside",_,[e("div",l,[d(s,{name:"info"})]),t(),r])])}const h=n(i,[["render",m],["__file","list-item.html.vue"]]);export{h as default};
