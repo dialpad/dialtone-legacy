3276188a683b37eb49605bd4ef4f0037014e069f
josedialpad <jose.silva@dialpad.com>
2023-01-19 17:55:28 +0000

    Deploying to gh-pages from @ dialpad/dialtone@d700ce8e3ce1e9913d6f9d5f79a88a965367e4be 🚀

---
+++ staging/assets/dropdown.html-ccb1d565.js
@@ -0,0 +1,3 @@
+import{_ as o,s,v as d,x as c,y as e,G as a,C as t}from"./framework-4a991d5d.js";const i={},_={class:"d-notice d-notice--info d-mt24 d-wmx100p",role:"status","aria-hidden":"false"},l={class:"d-notice__icon"},r=e("div",{class:"d-notice__content d-stack4"},[e("p",{class:"d-notice__message"},[t(`
+      Currently documented in `),e("a",{target:"_blank",href:"https://vue.dialpad.design/?path=/docs/components-dropdown--default",class:"d-link d-link--muted"},"Dialtone Vue"),t(`.
+    `)])],-1);function m(p,f){const n=s("dt-icon");return d(),c("div",null,[e("aside",_,[e("div",l,[a(n,{name:"info"})]),t(),r])])}const h=o(i,[["render",m],["__file","dropdown.html.vue"]]);export{h as default};
