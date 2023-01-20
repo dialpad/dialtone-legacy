b2ae3fc70b13ee044b9d409a94ac54e0da867cb1
Jose Silva <jose.silva@dialpad.com>
2023-01-20 11:45:46 -0300

    add doc for markdown render component

--- docs/.vuepress/baseComponents/MarkdownRender.vue
+++ docs/.vuepress/baseComponents/MarkdownRender.vue
@@ -3,6 +3,14 @@
 </template>
 
 <script setup>
+/**
+ * Use this component to render markdown content in a Vue SFC.
+ * Use only for complex cases where the built-in markdown renderer in MD files is not enough
+ * or you need a reusable component with props.
+ * There's a POC to include md files in another md page,
+ * see https://github.com/dialpad/dialtone/commit/8498ffaf89f2b305be33689b6b0ae34724ff0878
+ */
+
 import { computed } from 'vue';
 import markdownIt from 'markdown-it';
 
