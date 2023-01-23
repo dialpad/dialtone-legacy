f123831894ff89aa06818b06b5fb08e07688b694
francisrupert <francisrupert@users.noreply.github.com>
2023-01-23 17:55:07 +0000

    Deploying to gh-pages from @ dialpad/dialtone@4044c2a3b681a580a2d22574286bd16f9c003363 🚀

---
+++ deploy-previews/pr-799/assets/lazy-show.html-45790066.js
@@ -0,0 +1,5 @@
+import{_ as c,s as o,v as d,x as h,G as t,B as n,C as s}from"./framework-4a991d5d.js";const i={data(){return{isShown:!1}}};function u(w,l,f,p,e,m){const _=o("dt-button"),a=o("dt-lazy-show"),r=o("code-well-header");return d(),h("div",null,[t(r,null,{default:n(()=>[t(_,{onClick:l[0]||(l[0]=x=>e.isShown=!e.isShown)},{default:n(()=>[s(`
+    Toggle
+  `)]),_:1}),s(),t(a,{transition:"fade",show:e.isShown},{default:n(()=>[s(`
+    I'm Lazy!
+  `)]),_:1},8,["show"])]),_:1})])}const y=c(i,[["render",u],["__file","lazy-show.html.vue"]]);export{y as default};
