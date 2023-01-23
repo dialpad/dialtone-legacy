3f0d896c14e000e0bfc4ed4cc2dc50fa03c83a75
josedialpad <josedialpad@users.noreply.github.com>
2023-01-23 16:27:34 +0000

    Deploying to gh-pages from @ dialpad/dialtone@30a31bee93ec4b072a0472d6508b88fd03f902ca 🚀

---
+++ deploy-previews/pr-798/assets/emoji-text-wrapper.html-e6c1a721.js
@@ -0,0 +1,3 @@
+import{_ as r,s as e,v as c,x as _,G as t,B as o,C as l}from"./framework-4a991d5d.js";const s={};function i(m,d){const a=e("dt-emoji-text-wrapper"),n=e("code-well-header");return c(),_("div",null,[t(n,null,{default:o(()=>[t(a,null,{default:o(()=>[l(`
+    Some text with :invalid-emoji: :smile: :cry: and 😄, and custom emojis :octocat: :shipit:
+  `)]),_:1})]),_:1})])}const x=r(s,[["render",i],["__file","emoji-text-wrapper.html.vue"]]);export{x as default};
