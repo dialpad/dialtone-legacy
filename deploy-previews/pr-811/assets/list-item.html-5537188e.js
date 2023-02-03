1b3121570a4852696cffec88abc588a0da3793dd
braddialpad <braddialpad@users.noreply.github.com>
2023-02-03 05:42:18 +0000

    Deploying to gh-pages from @ dialpad/dialtone@60f7173678b8a5eba309c0229e2d25707111e90e 🚀

---
+++ deploy-previews/pr-811/assets/list-item.html-5537188e.js
@@ -0,0 +1,4 @@
+import{_,s as l,v as d,x as r,G as n,B as t,C as e,y as o}from"./framework-2b52b09a.js";const m={},u=o("span",null,null,-1),h=o("span",null,null,-1),p=o("h2",{id:"vue-api",tabindex:"-1",class:"d-docsite--header-2"},[e("Vue API "),o("a",{class:"header-anchor d-link d-docsite--link d-link",href:"#vue-api","aria-hidden":"true"},"#")],-1);function f(b,v){const a=l("dt-icon"),s=l("dt-list-item"),c=l("code-well-header"),i=l("component-vue-table");return d(),r("div",null,[n(c,{class:"d-d-block"},{default:t(()=>[o("ul",null,[n(s,{navigationType:"tab"},{left:t(()=>[n(a,{name:"check"})]),subtitle:t(()=>[h,e(`{subtitle}
+      `)]),bottom:t(()=>[e(`
+        {bottom}
+      `)]),right:t(()=>[n(a,{name:"external-link"})]),default:t(()=>[e(),u,e(),e(),e()]),_:1})])]),_:1}),e(),p,e(),n(i,{"component-name":"listitem"})])}const x=_(m,[["render",f],["__file","list-item.html.vue"]]);export{x as default};
