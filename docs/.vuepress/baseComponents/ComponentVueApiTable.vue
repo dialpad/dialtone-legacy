ab2bccaa46a3c7c4735d0ed5a0e9b2d96f99e929
Jose Silva <jose.silva@dialpad.com>
2023-02-13 14:49:55 -0300

    sort vue api table by name

--- docs/.vuepress/baseComponents/ComponentVueApiTable.vue
+++ docs/.vuepress/baseComponents/ComponentVueApiTable.vue
@@ -29,7 +29,7 @@
     </thead>
     <tbody>
       <tr
-        v-for="({ name, description, type, defaultValue }) in tableData"
+        v-for="({ name, description, type, defaultValue }) in sortedTableDataByName"
         :key="name"
       >
         <th
@@ -38,10 +38,12 @@
           v-text="name"
         />
         <td
-          v-if="description"
           class="d-ff-mono d-fs-100 vue-api-table"
         >
-          <div class="d-d-grid d-gg16 d-g-cols1">
+          <div
+            v-if="description"
+            class="d-d-grid d-gg16 d-g-cols1"
+          >
             <markdown-render :markdown="description" />
             <span v-if="type">
               <dt-badge>{{ type }}</dt-badge>
@@ -49,10 +51,12 @@
           </div>
         </td>
         <td
-          v-if="defaultValue"
+          v-if="withDefault"
           class="d-fs-100"
         >
-          <dt-badge>{{ defaultValue }}</dt-badge>
+          <dt-badge v-if="defaultValue">
+            {{ defaultValue }}
+          </dt-badge>
         </td>
       </tr>
     </tbody>
@@ -78,4 +82,22 @@ const props = defineProps({
 const withDefault = computed(() => {
   return props.tableData.some((item) => item.defaultValue);
 });
+
+const sortedTableDataByName = computed(() => {
+  if (!props.tableData) return null;
+
+  return sortDataByKey([...props.tableData], 'name');
+});
+
+const sortDataByKey = (data, key) => {
+  return data.sort((a, b) => {
+    if (a[key] < b[key]) {
+      return -1;
+    }
+    if (a[key] > b[key]) {
+      return 1;
+    }
+    return 0;
+  });
+};
 </script>
