3aa0dce42092a19a329f135ec1fa2bdf56228491
Julio Ortega <julio.ortega@dialpad.com>
2023-04-11 11:42:43 -0600

    fix position issues

--- docs/.vuepress/theme/components/PageToc.vue
+++ docs/.vuepress/theme/components/PageToc.vue
@@ -1,8 +1,6 @@
 <template>
-  <aside
-    class="vuepress-toc lg:d-ps-relative lg:d-w100p d-ps-fixed d-r16 d-pt24"
-  >
-    <h2 class="lg:d-pl12 d-headline-eyebrow d-fw-semibold">
+  <aside class="vuepress-toc lg:d-ps-relative lg:d-t0 lg:d-w100p d-ps-sticky d-t64 d-pt24 d-as-flex-start d-ga-toc">
+    <h2 class="d-headline-eyebrow d-fw-semibold">
       On this page
     </h2>
     <toc
