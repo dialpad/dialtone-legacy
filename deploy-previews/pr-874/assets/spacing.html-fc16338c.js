20a6aa31d99aac599f1e65b6529a93704cfbdc5d
francisrupert <francisrupert@users.noreply.github.com>
2023-05-05 18:18:29 +0000

    Deploying to gh-pages from @ dialpad/dialtone@958f6003b7a78cd352a611f619b949ff8e76a430 🚀

---
+++ deploy-previews/pr-874/assets/spacing.html-fc16338c.js
@@ -0,0 +1,1 @@
+import{_ as t,s as n,v as o,x as c,G as a}from"./framework-8a6d41ce.js";const s={};function _(r,l){const e=n("token-table");return o(),c("div",null,[a(e,{category:"space"})])}const m=t(s,[["render",_],["__file","spacing.html.vue"]]);export{m as default};
