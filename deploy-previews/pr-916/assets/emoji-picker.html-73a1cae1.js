f6466f50c646715de7df3f10578b93a97709a53a
iropolo <iropolo@users.noreply.github.com>
2023-06-28 15:48:25 +0000

    Deploying to gh-pages from @ dialpad/dialtone@1aec9391955f57d7d17261f495e50ded531542de 🚀

---
+++ deploy-previews/pr-916/assets/emoji-picker.html-73a1cae1.js
@@ -0,0 +1,29 @@
+import{_ as c,s as a,v as l,x as p,G as n,B as u,C as s,J as r}from"./framework-ecbb713b.js";const i={},k=r(`<div class="language-html" data-ext="html"><pre class="language-html"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>dt-emoji-picker</span>
+    <span class="token attr-name">skin-tone</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>skinTone<span class="token punctuation">&quot;</span></span>
+    <span class="token attr-name">tabSetLabels</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>[
+      &#39;Most recently used&#39;,
+      &#39;Smileys and people&#39;,
+      &#39;Nature&#39;,
+      &#39;Food&#39;,
+      &#39;Activity&#39;,
+      &#39;Travel&#39;,
+      &#39;Objects&#39;,
+      &#39;Symbols&#39;,
+      &#39;Flags&#39;,
+    ]<span class="token punctuation">&quot;</span></span>
+    <span class="token attr-name">skinSelectorButtonTooltipLabel</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>Change default skin tone<span class="token punctuation">&quot;</span></span>
+    <span class="token attr-name">searchNoResultsLabel</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>No results<span class="token punctuation">&quot;</span></span>
+    <span class="token attr-name">searchResultsLabel</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>Search results<span class="token punctuation">&quot;</span></span>
+    <span class="token attr-name">searchPlaceholderLabel</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>Search...<span class="token punctuation">&quot;</span></span>
+  <span class="token punctuation">/&gt;</span></span>
+</code></pre></div><h2 id="vue-api" tabindex="-1" class="d-docsite--header-2">Vue API <a class="header-anchor d-link d-docsite--link d-link" href="#vue-api" aria-hidden="true">#</a></h2>`,2);function d(m,h){const t=a("dt-emoji-pickerAAA"),e=a("code-well-header"),o=a("component-vue-api");return l(),p("div",null,[n(e,null,{default:u(()=>[n(t,{"skin-tone":"skinTone",tabSetLabels:`[
+      'Most recently used',
+      'Smileys and people',
+      'Nature',
+      'Food',
+      'Activity',
+      'Travel',
+      'Objects',
+      'Symbols',
+      'Flags',
+    ]`,skinSelectorButtonTooltipLabel:"Change default skin tone",searchNoResultsLabel:"No results",searchResultsLabel:"Search results",searchPlaceholderLabel:"Search..."})]),_:1}),s(),k,s(),n(o,{"component-name":"emojipicker"})])}const v=c(i,[["render",d],["__file","emoji-picker.html.vue"]]);export{v as default};
