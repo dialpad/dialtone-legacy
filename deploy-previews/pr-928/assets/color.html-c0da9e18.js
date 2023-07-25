9f7c24f7eb970ab281f0258628db71cff1ab60a3
iropolo <iropolo@users.noreply.github.com>
2023-07-25 22:11:15 +0000

    Deploying to gh-pages from @ dialpad/dialtone@6fad1413f10be69340648e17171b69d65b1f0919 🚀

---
+++ deploy-previews/pr-928/assets/color.html-c0da9e18.js
@@ -0,0 +1,1 @@
+import{_ as o,s as t,v as c,x as n,G as r}from"./framework-d5b0d6bb.js";const l={};function _(a,s){const e=t("token-table");return c(),n("div",null,[r(e,{category:"color"})])}const f=o(l,[["render",_],["__file","color.html.vue"]]);export{f as default};
---
+++ deploy-previews/pr-928/assets/typography.html-5bff8e86.js
@@ -0,0 +1,1 @@
+import{_ as t,s as o,v as n,x as a,G as r}from"./framework-d5b0d6bb.js";const c={};function _(s,l){const e=o("token-table");return n(),a("div",null,[r(e,{category:"typography"})])}const m=t(c,[["render",_],["__file","typography.html.vue"]]);export{m as default};
