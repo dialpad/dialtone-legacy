cbb025de30b23bb1228a3b992765b43b2f603cc2
juliodialpad <juliodialpad@users.noreply.github.com>
2023-08-03 15:47:22 +0000

    Deploying to gh-pages from @ dialpad/dialtone@8fd4bd9794f87b01bcb901fc9d2dbbc8241e7d46 🚀

---
+++ deploy-previews/pr-931/assets/spacing.html-037e1926.js
@@ -0,0 +1,1 @@
+import{_ as t,s as n,v as o,x as c,G as a}from"./framework-d5b0d6bb.js";const s={};function _(r,l){const e=n("token-table");return o(),c("div",null,[a(e,{category:"space"})])}const m=t(s,[["render",_],["__file","spacing.html.vue"]]);export{m as default};
