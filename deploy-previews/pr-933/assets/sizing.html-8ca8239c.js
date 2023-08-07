f6e9cbbc816d0a049c51905140dbee0afc149e60
juliodialpad <juliodialpad@users.noreply.github.com>
2023-08-07 18:15:19 +0000

    Deploying to gh-pages from @ dialpad/dialtone@2d62a3eaf317465526aa5836c6610885384bae12 🚀

---
+++ deploy-previews/pr-933/assets/sizing.html-8ca8239c.js
@@ -0,0 +1,1 @@
+import{_ as t,s as n,v as o,x as s,G as c}from"./framework-c7d7d111.js";const _={};function a(r,l){const e=n("token-table");return o(),s("div",null,[c(e,{category:"size"})])}const m=t(_,[["render",a],["__file","sizing.html.vue"]]);export{m as default};
