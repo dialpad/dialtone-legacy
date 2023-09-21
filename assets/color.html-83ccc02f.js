43c905c937e8e6761ef6af7d0e7af4282b18d625
braddialpad <brad.paugh@dialpad.com>
2023-09-21 17:29:19 +0000

    Deploying to gh-pages from @ dialpad/dialtone@2c5b014b68bcd942ccf8ab3e6b5930e040e03acf 🚀

---
+++ assets/color.html-83ccc02f.js
@@ -0,0 +1,1 @@
+import{_ as o,s as t,v as c,x as n,G as r}from"./framework-291274b0.js";const l={};function _(a,s){const e=t("token-table");return c(),n("div",null,[r(e,{category:"color"})])}const f=o(l,[["render",_],["__file","color.html.vue"]]);export{f as default};
