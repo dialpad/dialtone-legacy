f2dd6ced3582727f017f421329b2d4c0efe16dde
juliodialpad <julio.ortega@dialpad.com>
2023-09-27 17:39:51 +0000

    Deploying to gh-pages from @ dialpad/dialtone@551b4d58e736c7d4dc3ffbea71b53ced6830d861 🚀

--- assets/sizing.html-49f2ad5c.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,s as n,v as o,x as s,G as c}from"./framework-291274b0.js";const _={};function a(r,l){const e=n("token-table");return o(),s("div",null,[c(e,{category:"size"})])}const m=t(_,[["render",a],["__file","sizing.html.vue"]]);export{m as default};
--- assets/typography.html-bdea0c3b.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,s as o,v as n,x as a,G as r}from"./framework-291274b0.js";const c={};function _(s,l){const e=o("token-table");return n(),a("div",null,[r(e,{category:"typography"})])}const m=t(c,[["render",_],["__file","typography.html.vue"]]);export{m as default};
