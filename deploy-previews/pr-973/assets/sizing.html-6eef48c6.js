e6415dda84a9947b0cc43c13c23b73bf83956ef7
yorbi-dp <yorbi-dp@users.noreply.github.com>
2023-10-12 19:33:52 +0000

    Deploying to gh-pages from @ dialpad/dialtone@baa7ae40c744000b4f1173c2a15b917d4a52f4ca 🚀

---
+++ deploy-previews/pr-973/assets/sizing.html-6eef48c6.js
@@ -0,0 +1,1 @@
+import{_ as t,s as n,v as o,x as s,y as c}from"./framework-2f6ab852.js";const _={};function a(r,l){const e=n("token-table");return o(),s("div",null,[c(e,{category:"size"})])}const m=t(_,[["render",a],["__file","sizing.html.vue"]]);export{m as default};
