58b5bd3da05c12fb14d7432b9902ed0b23090bfc
Yorbi <yorbi.barriento@dialpad.com>
2023-10-23 12:16:06 -0400

    docs: update images with svgLoader component

--- docs/.vuepress/views/Overview.vue
+++ docs/.vuepress/views/Overview.vue
@@ -1,20 +1,18 @@
 <template>
   <div class="dialtone-wall">
-    <template
-      v-for="(page, index) in pages"
-      :key="page.title"
-    >
+    <template v-for="page in pages" :key="page.title">
       <component
         :is="cardElType(page)"
         :to="`/${basePath}/${page.link}/`"
         class="dialtone-wall__item"
       >
-        <div
-          v-if="page.thumb"
-          class="dialtone-wall__image"
-        >
-          <!--<div v-if="backgroundImage" class="dialtone-wall__background" v-html="backgroundImage"></div>-->
-          <div v-if="svgImage[index]" class="dialtone-wall__thumb" v-html="svgImage[index]"></div>
+        <div v-if="page.thumb" class="dialtone-wall__image">
+          <svg-loader class="dialtone-wall__thumb" :name="page.fileName" />
+          <!-- <img
+            class="dialtone-wall__thumb"
+            :alt="`${page.fileName}-thumbnail`"
+            :src="$withBase(`/assets/images/${basePath}/${page.fileName}.png`)"
+          > Fallback image -->
         </div>
         <div class="dialtone-wall__details">
           <div class="dialtone-wall__title">
@@ -37,8 +35,8 @@
 </template>
 
 <script setup>
-import { ref, onMounted, computed, defineProps } from 'vue';
-const props = defineProps({
+import SvgLoader from '../baseComponents/SvgLoader.vue';
+defineProps({
   pages: {
     type: Object,
     default: () => {},
@@ -60,46 +58,16 @@ const badgeKindClass = (status) => {
   }
 };
 const pageTitle = (page) => {
-  const shortTitle = page.shortTitle ? page.shortTitle[0].toUpperCase() + page.shortTitle.slice(1) : undefined;
+  const shortTitle = page.shortTitle
+    ? page.shortTitle[0].toUpperCase() + page.shortTitle.slice(1)
+    : undefined;
   return shortTitle || page.title;
 };
 const cardElType = (page) => {
-  if (page.status !== 'planned' || (page.storybook && page.storybook !== 'planned')) return 'router-link';
+  if (
+    page.status !== 'planned' ||
+    (page.storybook && page.storybook !== 'planned')
+  ) { return 'router-link'; }
   return 'div';
 };
-
-const svgPaths = computed(() => {
-    return props.pages.map(page => `/assets/images/${props.basePath}/${page.fileName}.svg`);
-});
-
-const svgImage = ref([]);
-const backgroundImage = ref('');
-
-const fetchBackgroundSvg = async () => {
-  try {
-    const response = await fetch(`/assets/images/${props.basePath}/img-background.svg`);
-    backgroundImage.value = await response.text();
-  } catch (error) {
-    console.error("Failed to fetch background SVG:", error);
-  }
-};
-
-const fetchSvgs = async () => {
-    const contents = [];
-    for (const path of svgPaths.value) {
-        try {
-            const response = await fetch(path);
-            const content = await response.text();
-            contents.push(content);
-        } catch (error) {
-            console.error(`Failed to fetch SVG at ${path}:`, error);
-        }
-    }
-    svgImage.value = contents;
-};
-
-onMounted(() => {
-  fetchBackgroundSvg();
-  fetchSvgs();
-});
 </script>
