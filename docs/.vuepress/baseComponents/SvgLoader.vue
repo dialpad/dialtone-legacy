99e8eb0644adf8f85a5fe353a11fa950fb9823ee
Yorbi <144369133+yorbi-dp@users.noreply.github.com>
2023-10-24 13:44:20 -0400

    docs: update images in components overview for light and dark support (#973)
    * docs: update images in components overview for light and dark support
    
    * docs: update images with svgLoader component
    
    * fix SvgLoader
    
    * remove foundSvg
    
    * docs: update the svg images size for components overview
    
    * docs: removed ids on svg tags
    
    ---------
    
    Co-authored-by: Brad Paugh <brad.paugh@dialpad.com>

--- docs/.vuepress/baseComponents/SvgLoader.vue
+++ docs/.vuepress/baseComponents/SvgLoader.vue
@@ -1,5 +1,7 @@
 <template>
-  <component :is="svg" />
+  <template v-for="svg in svgs" :key="svg">
+    <component :is="svg" v-if="svg" />
+  </template>
 </template>
 
 <script setup>
@@ -7,11 +9,27 @@ import { defineAsyncComponent } from 'vue';
 
 // render an svg by name
 const props = defineProps({
+  /**
+   * Filename of the svg, without extension.
+   */
   name: {
     type: String,
     required: true,
   },
 });
 
-const svg = defineAsyncComponent(() => import(`../../assets/svg/${props.name}.svg?component`));
+const svgs = [
+  defineAsyncComponent({
+    loader: () => import(`../public/assets/images/${props.name}.svg?component`),
+    onError: () => {},
+  }),
+  defineAsyncComponent({
+    loader: () => import(`../public/assets/images/components/${props.name}.svg?component`),
+    onError: () => {},
+  }),
+  defineAsyncComponent({
+    loader: () => import(`../public/assets/images/favicons/${props.name}.svg?component`),
+    onError: () => {},
+  }),
+];
 </script>
