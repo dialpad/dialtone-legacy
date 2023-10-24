175b41f656f8e077fe3449555c62e915e07614b3
Nina <ninamrepetto@gmail.com>
2023-10-24 13:30:46 -0300

    fix for code review

--- docs/.vuepress/baseComponents/TokenExample.vue
+++ docs/.vuepress/baseComponents/TokenExample.vue
@@ -2,28 +2,28 @@
   <div
     v-if="category === 'color'"
     class="d-bar-circle d-w42 d-h42"
-    :style="getColorStyle()"
+    :style="getColorStyle"
   />
   <p
     v-if="category === 'typography'"
-    :style="getTypographyStyle()"
+    :style="getTypographyStyle"
   >
     Aa
   </p>
   <div
     v-if="category === 'shadow'"
     class="d-bar2 d-w42 d-h42"
-    :style="getShadowStyle()"
+    :style="getShadowStyle"
   />
   <div
     v-if="category === 'size'"
     class="rectangle"
-    :style="getSizeStyle()"
+    :style="getSizeStyle"
   />
   <div
     v-if="category === 'space'"
     class="rectangle"
-    :style="getSizeStyle()"
+    :style="getSizeStyle"
   />
 </template>
 
@@ -39,6 +39,9 @@ const TYPOGRAPHY_KEY_MAP = {
 };
 
 const SHADOW_COMPOSITION_TOKENS = ['small', 'medium', 'large', 'extra-large', 'card', 'focus', 'focus-inset'];
+
+const isTypography = (name, key) => name.includes('--dt-typography') && name.includes(key);
+const isFont = (name, key) => name.includes(`--dt-font-${key}`);
 
 export default {
   name: 'TokenExample',
@@ -61,45 +64,44 @@ export default {
     },
   },
 
-  methods: {
+  computed: {
     getColorStyle () {
-      const property = /^var\(--dt-(\w+)-/.exec(this.name)[1];
+      const property = this.name.split('--dt-')[1]?.split('-')[0];
       switch (property) {
         case 'color':
-          return `background: ${this.value}`;
+        case 'theme':
+          return { background: this.value };
 
         case 'opacity':
-          return `background: rgba(0, 0, 0, ${this.value})`;
-
-        case 'theme':
-          return `background: ${this.value}`;
+          return { background: `rgba(0, 0, 0, ${this.value})` };
 
         default:
           return null;
       }
     },
 
+    getComposedTypographyStyle () {
+      if (this.name.startsWith('var(--dt-typography')) {
+        return `font: ${this.value}`;
+      }
+      return null;
+    },
+
     getTypographyStyle () {
-      const fontRegex = new RegExp(`^var\\(--dt-font-(${Object.keys(TYPOGRAPHY_KEY_MAP).join('|')})`);
-      const fontProperties = fontRegex.exec(this.name);
-      if (fontProperties) {
-        return `${TYPOGRAPHY_KEY_MAP[fontProperties[1]]}: ${this.value}`;
+      for (const key in TYPOGRAPHY_KEY_MAP) {
+        if (isFont(this.name, key) || isTypography(this.name, key)) {
+          return { [TYPOGRAPHY_KEY_MAP[key]]: this.value };
+        }
       }
-      const typographyRegex = new RegExp(`^var\\(--dt-typography-.+(${Object.keys(TYPOGRAPHY_KEY_MAP).join('|')})\\)$`);
-      const typographyProperties = typographyRegex.exec(this.name);
-      if (typographyProperties) {
-        return `${TYPOGRAPHY_KEY_MAP[typographyProperties[1]]}: ${this.value}`;
-      }
-      const isCompositionToken = this.name.startsWith('var(--dt-typography');
-      if (isCompositionToken) {
+      if (this.name.startsWith('var(--dt-typography')) {
         return `font: ${this.value}`;
       }
       return null;
     },
 
     getShadowStyle () {
-      if (SHADOW_COMPOSITION_TOKENS.find(name => this.name.endsWith(`${name})`))) {
-        return `box-shadow: ${this.value}`;
+      if (SHADOW_COMPOSITION_TOKENS.some(name => this.name.endsWith(`${name})`))) {
+        return { 'box-shadow': this.value };
       }
       return null;
     },
@@ -107,7 +109,7 @@ export default {
     getSizeStyle () {
       if (this.value.endsWith('%')) return null;
       const value = parseFloat(this.value.replace('rem', ''));
-      if (value < 12.8 && value > 0) return `width: ${this.value}`;
+      if (value < 12.8 && value > 0) return { width: this.value };
       return null;
     },
   },
